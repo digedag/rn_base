df86f613b43499042b234f7868db6dfc529dbf8a
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2008-05-01 11:34:33 +0000

    Initial version

---
+++ util/class.tx_rnbase_util_FormTool.php
@@ -0,0 +1,261 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2007 Rene Nitzsche (rene@system25.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once (PATH_t3lib.'class.t3lib_tceforms.php');
+
+/**
+ * Diese Klasse stellt hilfreiche Funktionen zur Erstellung von Formularen
+ * im Backend zur Verfügung
+ */
+class tx_rnbase_util_FormTool {
+  var $form; // TCEform-Instanz
+
+  function init($doc) {
+    global $BACK_PATH;
+    $this->doc = $doc;
+
+    // TCEform für das Formular erstellen
+    $this->form = t3lib_div::makeInstance('t3lib_TCEforms');
+    $this->form->initDefaultBEmode();
+    $this->form->backPath = $BACK_PATH;
+  }
+
+  /**
+   * Erstellt einen Link zur Bearbeitung eines Datensatzes
+   * @param $editTable DB-Tabelle des Datensatzes
+   * @param $editUid UID des Datensatzes
+   * @param $label Bezeichnung des Links
+   */
+  function createEditLink($editTable, $editUid, $label = 'Edit') {
+    $params = '&edit['.$editTable.']['.$editUid.']=edit';
+    return '<a href="#" onclick="'.htmlspecialchars(t3lib_BEfunc::editOnClick($params,$GLOBALS['BACK_PATH'])).'">'.
+     '<img'.t3lib_iconWorks::skinImg($GLOBALS['BACK_PATH'],'gfx/edit2.gif','width="11" height="12"').' title="Edit UID: '.$editUid.'" border="0" alt="" />'.
+     $label .'</a>';
+  }
+
+  /**
+   * Erstellt einen Link zur Anzeige von Informationen über einen Datensatz
+   * @param $editTable DB-Tabelle des Datensatzes
+   * @param $editUid UID des Datensatzes
+   * @param $label Bezeichnung des Links
+   */
+  function createInfoLink($editTable, $editUid, $label = 'Info') {
+//    $params = '&edit['.$editTable.']['.$editUid.']=edit';
+    return '<a href="#" onclick="top.launchView(' . "'" . $editTable . "', ' " . $editUid . "'); return false;" . '">'.
+     '<img'.t3lib_iconWorks::skinImg($GLOBALS['BACK_PATH'],'gfx/zoom2.gif','width="11" height="12"').' title="UID: '.$editUid.'" border="0" alt="" />'.
+     $label .'</a><br/>';
+  }
+
+  /**
+   * Erstellt einen Link zum Verschieben eines Datensatzes auf eine andere Seite
+   * @param $editTable DB-Tabelle des Datensatzes
+   * @param $recordUid UID des Datensatzes
+   * @param $currentPid PID der aktuellen Seite des Datensatzes
+   * @param $label Bezeichnung des Links
+   */
+  function createMoveLink($editTable, $recordUid, $currentPid, $label = 'Move') {
+
+    return "<a href=\"#\" onclick=\"return jumpSelf('/typo3/db_list.php?id=". $currentPid ."&amp;CB[el][" . $editTable
+           . "%7C" . $recordUid . "]=1');\"><img " .t3lib_iconWorks::skinImg($GLOBALS['BACK_PATH'],'gfx/clip_cut.gif','width="16" height="16"'). ' title="UID: '. $recordUid . '" alt="" />' . $label .'</a>';
+
+  }
+
+  /**
+   * Erstellt einen Link auf die aktuelle Location mit zusätzlichen Parametern
+   */
+  function createLink($params, $pid, $label) {
+
+    return '<a href="#" onclick="'.htmlspecialchars("window.location.href='index.php?id=".$pid . $params)."'; return false;\">".
+       $label .'</a>';
+  }
+
+
+  function createHidden($name, $value){
+    return '<input type="hidden" name="'. $name.'" value="' . $value . '" />';
+  }
+
+	function createRadio($name, $value, $checked = false, $onclick = ''){
+		return '<input type="radio" name="'. $name.'" value="' . $value . '" '. ($checked ? 'checked="checked"' : '') . (strlen($onclick) ? ' onclick="' . $onclick . '"' : '') . ' />';
+	}
+
+	function createCheckbox($name, $value, $checked = false, $onclick = ''){
+		return '<input type="checkbox" name="'. $name.'" value="' . $value . '" '. ($checked ? 'checked="checked"' : '') . (strlen($onclick) ? ' onclick="' . $onclick . '"' : '') .' />';
+	}
+  
+  function createSubmit($name, $value, $confirmMsg = ''){
+    $btn = '<input type="submit" name="'. $name.'" value="' . $value . '" ';
+    if(strlen($confirmMsg)) 
+    	$btn .= 'onclick="return confirm('.$GLOBALS['LANG']->JScharCode($confirmMsg).')"';
+    $btn .= '/>';
+    return $btn;
+  }
+  
+  /**
+   * Erstellt ein Textarea
+   */
+  function createTextArea($name, $value, $cols='30', $rows='5'){
+    return '
+      <textarea name="' . $name . '" style="width:288px;" class="formField1"'.
+      ' cols="'.$cols.'" rows="'.$rows.'" wrap="virtual">' . $value . '</textarea>';
+  }
+
+  /**
+   * Erstellt ein einfaches Textfield
+   */
+  function createTxtInput($name, $value, $width){
+    return '<input type="text" name="'. $name.'"'.$this->doc->formWidth($width).' value="' . $value . '" />';
+  }
+
+  /**
+   * Erstellt ein Eingabefeld für Integers
+   */
+  function createIntInput($name, $value, $width, $maxlength=10){
+//t3lib_div::debug($GLOBALS['TBE_TEMPLATE']->formWidth(10), 'form');
+//      <input type="text" name="' . $name . '_hr"'.$this->doc->formWidth($width).
+
+    $out = '
+      <input type="text" name="' . $name . '_hr"'.$GLOBALS['TBE_TEMPLATE']->formWidth($width).
+      ' onchange="typo3FormFieldGet(\'' . $name . '\', \'int\', \'\', 0,0);"'.
+      $GLOBALS['TBE_TEMPLATE']->formWidth(12). ' maxlength="' . $maxlength . '"/>'.' 
+      <input type="hidden" value="'.htmlspecialchars($value).'" name="' . $name . '" />';
+
+    // JS-Code für die Initialisierung im TCEform eintragen
+    $this->form->extJSCODE .= 'typo3FormFieldSet("' . $name . '", "int", "", 0,0);';
+
+
+    return $out;
+  }
+
+  /**
+   * Erstellt ein Eingabefeld für DateTime
+   */
+  function createDateInput($name, $value){
+
+    $out = '
+      <input type="text" name="' . $name . '_hr" 
+        onchange="typo3FormFieldGet(\'' . $name . '\', \'datetime\', \'\', 0,0);"'.
+      $GLOBALS['TBE_TEMPLATE']->formWidth(11).
+    ' />'.'
+      <input type="hidden" value="'.htmlspecialchars($value).'" name="' . $name . '" />';
+
+    // JS-Code für die Initialisierung im TCEform eintragen
+    $this->form->extJSCODE .= 'typo3FormFieldSet("' . $name . '", "datetime", "", 0,0);';
+
+    return $out;
+  }
+
+  /**
+   * Erstellt eine Selectbox mit festen Werten in der TCA. 
+   * Die Labels werden in der richtigen Sprache angezeigt.
+   */
+  function createSelectSingle($name, $value, $table, $column){
+    global $TCA, $LANG;
+
+    $out = '
+      <select name="' . $name . '" class="select"> 
+    ';
+    // Die TCA laden
+    t3lib_div::loadTCA($table);
+
+    // Die Options ermitteln
+    foreach($TCA[$table]['columns'][$column]['config']['items'] As $item){
+      $sel = '';
+      if (intval($value) == intval($item[1])) $sel = 'selected="selected"';
+      $out .= '<option value="' . $item[1] . '" ' . $sel . '>' . $LANG->sL($item[0]) . '</option>';
+    }
+    $out .= '
+      </select>
+    ';
+    return $out;
+  }
+
+  /**
+   * Erstellt eine Select-Box aus dem übergebenen Array
+   */
+  function createSelectSingleByArray($name, $value, $arr, $reload=0){
+    $reloadStr = $reload ? ' onChange="this.form.submit()" ' : '';
+
+    $out = '
+      <select name="' . $name . '" class="select"' . $reloadStr . '> 
+    ';
+    
+    // Die Options ermitteln
+    foreach($arr As $key => $val){
+      $sel = '';
+      if (intval($value) == intval($key)) $sel = 'selected="selected"';
+      $out .= '<option value="' . $key . '" ' . $sel . '>' . $val . '</option>';
+    }
+    $out .= '
+      </select>
+    ';
+    return $out;
+  }
+
+  /**
+   * @param $pid ID der aktuellen Seite
+   */
+  function getJSCode($pid) {
+    // Add JavaScript functions to the page:
+    $JScode=$this->doc->wrapScriptTags('
+      function jumpToUrl(URL) {       //
+        window.location.href = URL;
+        return false;
+      }
+      function jumpExt(URL,anchor)    {       //
+        var anc = anchor?anchor:"";
+        window.location.href = URL+(T3_THIS_LOCATION?"&returnUrl="+T3_THIS_LOCATION:"")+anc;
+        return false;
+      }
+      function jumpSelf(URL)  {       //
+        window.location.href = URL+(T3_RETURN_URL?"&returnUrl="+T3_RETURN_URL:"");
+        return false;
+      }
+
+      function setHighlight(id)       {       //
+        top.fsMod.recentIds["web"]=id;
+        top.fsMod.navFrameHighlightedID["web"]="pages"+id+"_"+top.fsMod.currentBank;    // For highlighting
+
+        if (top.content && top.content.nav_frame && top.content.nav_frame.refresh_nav)  {
+                top.content.nav_frame.refresh_nav();
+        }
+      }
+      var T3_RETURN_URL = "";
+      var T3_THIS_LOCATION = "db_list.php%3Fid%3D' . $pid . '%26table%3D";');
+
+
+    // Setting up the context sensitive menu:
+//    $CMparts=$this->doc->getContextMenuCode();
+//    $this->doc->bodyTagAdditions = $CMparts[1];
+//    $JScode.=$CMparts[0];
+//    $this->doc->postCode.= $CMparts[2];
+    return $JScode;
+  }
+
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/class.tx_cfcleague_form_tool.php'])	{
+  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/class.tx_cfcleague_form_tool.php']);
+}
+
+?>
---
+++ util/class.tx_rnbase_util_Link.php
@@ -0,0 +1,447 @@
+<?php
+
+/**
+ * This class is a wrapper around tslib_cObj::typoLink
+ *
+ * PHP versions 4 and 5
+ *
+ *  (c) 2008 Rene Nitzsche
+ *  Contact: rene@system25.de
+ *
+ *  Original version:
+ * Copyright (c) 2006-2007 Elmar Hinz
+ *
+ * LICENSE:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+/**
+ * This class is a wrapper around tslib_cObj::typoLink
+ *
+ * It is not a full implementation of typolink functionality
+ * but targeted to the day-to-day requirements. The idea is to provide
+ * an simple to use object orientated interface as an alternative to the
+ * typolink functions of pi_base.
+ *
+ * Depends on: the TS link function
+ *
+ * @author     Elmar Hinz <elmar.hinz@team-red.net>
+ * @package    TYPO3
+ * @subpackage lib
+ */
+class tx_lib_link{
+
+	var $tagAttributes = array();       // setting attributes for the tag in general
+	var $classString = '';              // tags class attribute
+	var $idString = '';                 // tags id attribute
+	var $cObject;                       // instance of tslib_cObj
+	var $destination = '';              // page id, alias, external link, etc.
+	var $labelString = '';              // tags label
+	var $labelHasAlreadyHtmlSpecialChars = false; // is the label already HSC?
+	var $noCacheBoolean = false;        // don't make a cHash
+	var $noHashBoolean = false;         // add a no_cache=1 parameter
+	var $overruledParameters = array(); // parameters overruled by $parameters
+	var $parameters = array();		      // parameters of the link
+	var $designatorString = '';         // parameter array name (prefixId) as controller namespace
+	var $anchorString = '';             // section anchor as url target
+	var $targetString = '';             // tags target attribute
+	var $externalTargetString = '_blank'; // external target defaults to new window
+	var $titleString = '';              // tags title attribute
+	var $titleHasAlreadyHtmlSpecialChars = false; //is title attribute already HSC?
+
+	// -------------------------------------------------------------------------------------
+	// Constructor
+	// -------------------------------------------------------------------------------------
+
+	/**
+	 * Construct a link object
+	 *
+	 * By default this object wraps tslib_cObj::typolink();
+	 * The $cObjectClass parameter can be used to provide a mock object
+	 * for unit tests.
+	 *
+	 * @param	object		mock object for testing purpuses
+	 * @return	void
+	 */
+	function tx_lib_link($cObjectClass = 'tslib_cObj') {
+		$this->cObject = t3lib_div::makeInstance($cObjectClass);
+	}
+
+	// -------------------------------------------------------------------------------------
+	// Setters
+	// -------------------------------------------------------------------------------------
+
+	/**
+	 * Set the section anchor of the url
+	 *
+	 * Anchor of page as url target.
+	 *
+	 * @param	string		the anchor
+	 * @return	object		self
+	 */
+	function anchor($anchorString) {
+		$this->anchorString = $anchorString;
+		return $this;
+	}
+
+	/**
+	 * Set the designator (parameter array name) as controler namespace
+	 *
+	 * Put the parameters into this array.
+	 * <samp>Example: &tx_example[parameterName]=parameterValue</samp>
+	 * tx_example is the designator, parameterName is the key,
+	 * pararmeterValue is the value of one array element.
+	 *
+	 * @param	string		parameter array name
+	 * @return	object		self
+	 */
+	function designator($designatorString) {
+		$this->designatorString = $designatorString;
+		return $this;
+	}
+
+	/**
+	 * Set the id attribute of the tag
+	 *
+	 * @param	string		id attribute
+	 * @return	object		self
+	 */
+	function idAttribute($idString) {
+		$this->idString = $idString;
+		return $this;
+	}
+
+	/**
+	 * Set the class attribute of the tag
+	 *
+	 * @param	string		class name
+	 * @return	object		self
+	 */
+	function classAttribute($classString) {
+		$this->classString = $classString;
+		return $this;
+	}
+
+	/**
+	 * Set the links destination
+	 *
+	 * @param	mixed		pageId, page alias, external url, etc.
+	 * @param	boolean		if true don't parse through htmlspecialchars()
+	 * @return	object		self
+	 * @see		TSref => typolink => parameter
+	 * @see		tslib_cObj::typoLink()
+	 */
+	function destination($destination) {
+		$this->destination = $destination;
+		return $this;
+	}
+
+	/**
+	 * Add no_cache=1 and disable the cHash parameter
+	 *
+	 * @param	boolean		if true don't make a cHash, set no_cache=1
+	 * @return	object		self
+	 */
+	function noCache() {
+		$this->noCacheBoolean = true;
+		return $this;
+	}
+
+	/**
+	 * Disable the cHash parameter
+	 *
+	 * @param	boolean		if true don't make a cHash
+	 * @return	object		self
+	 */
+	function noHash() {
+		$this->noHashBoolean = true;
+		return $this;
+	}
+
+	/**
+	 * Set the links label
+	 *
+	 * By default the label will be parsed through htmlspecialchars().
+	 *
+	 * @param	string		the label
+	 * @param	boolean		if true don't parse through htmlspecialchars()
+	 * @return	object		self
+	 */
+	function label($labelString, $hasAlreadyHtmlSpecialChars = false) {
+		$this->labelString = $labelString;
+		$this->labelHasAlreadyHtmlSpecialChars = $hasAlreadyHtmlSpecialChars;
+		return $this;
+	}
+
+	/**
+	 * Set array of parameters to be overruled by parameters
+	 *
+	 * The parameters will create a common array with the name $this->designatorString.
+	 * <samp>Example: &tx_example[parameterName]=parameterValue</samp>
+	 * tx_example is the designator, parameterName is the key,
+	 * pararmeterValue is the value of one array element.
+	 *
+	 * Usually you set the incomming piVars here you wan't to forward.
+	 * Like in tslib_pibase::pi_linkTP_keepPIvars the element DATA is unset during processing.
+	 *
+	 * @param	mixed		parameters
+	 * @return	object		self
+	 */
+	function overruled($overruledParameters = array()) {
+		if(is_object($overruledParameters)) {
+			$overruledParameters = $overruledParameters->getArrayCopy();
+		}
+		$this->overruledParameters = $overruledParameters;
+		return $this;
+	}
+
+	/**
+	 * Set array of new parameters to add to the link url
+	 *
+	 * The parameters will create a common array with the name $this->designatorString.
+	 * <samp>Example: &tx_example[parameterName]=parameterValue</samp>
+	 * tx_example is the designator, parameterName is the key,
+	 * pararmeterValue is the value of one array element.
+	 *
+	 * This parameters overrule parameters in $this->baseParameters.
+	 *
+	 * @param	mixed		parameters
+	 * @return	object		self
+	 */
+	function parameters($parameters = array()) {
+		if(is_object($parameters)) {
+			$parameters = $parameters->getArrayCopy();
+		}
+		$this->parameters = $parameters;
+		return $this;
+	}
+
+	/**
+	 * Set the attributes of the tag
+	 *
+	 * This is a general approach to set tag attributes by an array hash.
+	 *
+	 * @see	classAttribute()
+	 * @see	titleAttribute()
+	 * @see	targetAttribute()
+	 *
+	 * @param	array		key value pairs
+	 * @return	object		self
+	 */
+	function attributes($tagAttributes = array()) {
+		$this->tagAttributes = $tagAttributes;
+		return $this;
+	}
+
+	/**
+	 * Set target attribute of the tag
+	 * A shortcut for the targetAttribute() function.
+	 *
+	 * @see	targetAttribute()
+	 *
+	 * @param	string		target attribute
+	 * @return	object		self
+	 */
+	function target($targetString) {
+		$this->targetAttribute($targetString);
+		return $this;
+	}
+
+	/**
+	 * Set target attribute of the tag
+	 *
+	 * @param	string		target attribute
+	 * @return	object		self
+	 */
+	function targetAttribute($targetString) {
+		$this->targetString = $targetString;
+		return $this;
+	}
+
+	/**
+	 * Set external target attribute of the tag
+	 * Defaults to _blank
+	 *
+	 * @param	string		external target attribute
+	 * @return	object		self
+	 */
+	function externalTargetAttribute($targetString) {
+		$this->externalTargetString = $targetString;
+		return $this;
+	}
+
+	/**
+	 * Set title attribute of the tag
+	 * A shortcut for the titleAttribute() function.
+	 *
+	 * @see	titleAttribute()
+	 *
+	 * @param	string		title attribute
+	 * @param	boolean		if true don't apply htmlspecialchars() again
+	 * @return	object		self
+	 */
+	function title($titleString, $hasAlreadyHtmlSpecialChars = false) {
+		$this->titleAttribute($titleString, $hasAlreadyHtmlSpecialChars);
+		return $this;
+	}
+
+	/**
+	 * Set title attribute of the tag
+	 *
+	 * @param	string		title attribute
+	 * @param	boolean		if true don't apply htmlspecialchars() again
+	 * @return	object		self
+	 */
+	function titleAttribute($titleString, $hasAlreadyHtmlSpecialChars = false) {
+		$this->titleString = $titleString;
+		$this->titleHasAlreadyHtmlSpecialChars = $hasAlreadyHtmlSpecialChars;
+		return $this;
+	}
+
+	// -------------------------------------------------------------------------------------
+	// Getters
+	// -------------------------------------------------------------------------------------
+
+	/**
+	 * Return the link as tag
+	 *
+	 * @return	string		the link tag
+	 */
+	function makeTag() {
+		return $this->cObject->typolink(
+			$this->_makeLabel(),
+			$this->_makeConfig('tag')
+		);
+	}
+
+	/**
+	 * Return the link as url
+	 *
+	 * @param	boolean		set to true to run htmlspecialchars() on generated url
+	 * @return	string		the link url
+	 */
+	function makeUrl($applyHtmlspecialchars = TRUE) {
+		$url = $this->cObject->typolink(null, $this->_makeConfig('url'));
+		return $applyHtmlspecialchars ? htmlspecialchars($url) : $url;
+	}
+
+	/**
+	 * Redirect the page to the url
+	 *
+	 * @return	void
+	 */
+	function redirect() {
+		session_write_close();
+		header('Location: ' . t3lib_div::getIndpEnv('TYPO3_REQUEST_DIR') 
+			. $this->cObject->typolink(null, $this->_makeConfig('url')));
+		exit();
+	}
+
+	// -------------------------------------------------------------------------------------
+	// Private functions
+	// -------------------------------------------------------------------------------------
+
+	/**
+	 * Make the full configuration for the typolink function
+	 *
+	 * @param	string		$type: tag oder url
+	 * @return	array		the configuration
+	 * @access	private
+	 */
+	function _makeConfig($type) {
+		$conf = Array();
+		$this->parameters = is_array($this->parameters) ?
+			$this->parameters : array();
+		$this->overruledParameters = is_array($this->overruledParameters) ?
+			$this->overruledParameters : array();
+		unset($this->overruledParameters['DATA']);
+		$parameters
+			= t3lib_div::array_merge_recursive_overrule($this->overruledParameters,
+					$this->parameters);
+		foreach((array) $parameters as $key => $value) {
+			if(!is_array($value)) {   // TODO handle arrays
+				if($this->designatorString) {
+					$conf['additionalParams']
+						.= '&' . rawurlencode( $this->designatorString . '[' . $key . ']') . '=' . rawurlencode($value);
+				} else {
+					$conf['additionalParams'] .= '&' . rawurlencode($key) . '=' . rawurlencode($value);
+				}
+			}
+		}
+		if($this->noHashBoolean ) {
+			$conf['useCacheHash'] = 0;
+		} else {
+			$conf['useCacheHash'] = 1;
+		}
+		if($this->noCacheBoolean) {
+			$conf['no_cache'] = 1;
+			$conf['useCacheHash'] = 0;
+		} else {
+			$conf['no_cache'] = 0;
+		}
+		if($this->destination !== '')
+			$conf['parameter'] = $this->destination;
+		if($type == 'url') {
+			$conf['returnLast'] = 'url';
+		}
+		if($this->anchorString) {
+			$conf['section'] = $this->anchorString;
+		}
+		if($this->targetString) {
+			$conf['target'] = $this->targetString;
+		}
+		if($this->externalTargetString) {
+			$conf['extTarget'] = $this->externalTargetString;
+		}
+		if($this->classString) {
+			$conf['ATagParams'] .= 'class="' . $this->classString . '" ';
+		}
+		if($this->idString) {
+			$conf['ATagParams'] .= 'id="' . $this->idString . '" ';
+		}
+		if($this->titleString) {
+			$title = ($this->titleHasAlreadyHtmlSpecialChars) ? $this->titleString
+				: htmlspecialchars($this->titleString);
+			$conf['ATagParams'] .= 'title="' . $title . '" ';
+		}
+		if(is_array($this->tagAttributes)
+				&& (count($this->tagAttributes) > 0)) {
+			foreach($this->tagAttributes as $key => $value) {
+				$conf['ATagParams'] .= ' ' .  $key . '="' . htmlspecialchars($value) . '" ';
+			}
+		}
+		return $conf;
+	}
+
+	/**
+	 * Make the label for the link
+	 *
+	 * @return	string		the label
+	 * @access	private
+	 */
+	function _makeLabel() {
+		return ($this->labelHasAlreadyHtmlSpecialChars) ? $this->labelString
+			: htmlspecialchars($this->labelString);
+	}
+
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/lib/class.tx_lib_link.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/lib/class.tx_lib_link.php']);
+}
+
+?>
