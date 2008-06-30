e700dc23bfaf3675fe159133aec990f44e6bfdbc
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2008-06-30 19:31:58 +0000

    XClass definition fixed

--- util/class.tx_rnbase_util_FormTool.php
+++ util/class.tx_rnbase_util_FormTool.php
@@ -289,8 +289,8 @@ class tx_rnbase_util_FormTool {
 
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/class.tx_cfcleague_form_tool.php'])	{
-  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/class.tx_cfcleague_form_tool.php']);
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/util/class.tx_rnbase_util_FormTool.php'])	{
+  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/util/class.tx_rnbase_util_FormTool.php']);
 }
 
 ?>
