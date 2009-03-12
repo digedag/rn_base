0c296238b896d95ccee857a82e4872ae02673215
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2009-03-12 09:59:52 +0000

    Testcase für Dates

---
+++ tests/class.tx_rnbase_tests_dates_testcase.php
@@ -0,0 +1,45 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Rene Nitzsche (rene@system25.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('div') . 'class.tx_div.php');
+tx_div::load('tx_rnbase_util_Dates');
+
+class tx_rnbase_tests_dates_testcase extends tx_phpunit_testcase {
+
+	function test_dateConv() {
+		$zeit1 = '2009-02-11';
+		$tstamp1 = tx_rnbase_util_Dates::date_mysql2tstamp($zeit1);
+		$zeit2 = tx_rnbase_util_Dates::date_tstamp2mysql($tstamp1);
+
+//		$sDate = gmstrftime("%d.%m.%Y", $tstamp1);
+//		t3lib_div::debug($sDate, 'tx_rnbase_tests_dates_testcase :: test_dateConv'); // TODO: remove me
+		$this->assertEquals($zeit1, $zeit2);
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/tests/class.tx_rnbase_tests_dates_testcase.php']) {
+  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/tests/class.tx_rnbase_tests_dates_testcase.php']);
+}
+
+?>
