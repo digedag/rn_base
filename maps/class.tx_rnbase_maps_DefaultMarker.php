83eb95a987cb9147e34955d39db76ac4d3a6ffbd
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2009-05-22 13:24:14 +0000

    XClass fixed

--- maps/class.tx_rnbase_maps_DefaultMarker.php
+++ maps/class.tx_rnbase_maps_DefaultMarker.php
@@ -160,8 +160,8 @@ class tx_rnbase_maps_DefaultMarker implements tx_rnbase_maps_IMarker {
 	}
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/maps/class.tx_rnbase_maps_IMarker.php']) {
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/maps/class.tx_rnbase_maps_IMarker.php']);
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/maps/class.tx_rnbase_maps_DefaultMarker.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/maps/class.tx_rnbase_maps_DefaultMarker.php']);
 }
 
 ?>
