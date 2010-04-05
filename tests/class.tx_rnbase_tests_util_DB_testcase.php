199b6713ccded360d3139569f11596502b717617
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2010-04-05 14:42:39 +0000

    Testclass renamed

--- tests/class.tx_rnbase_tests_util_DB_testcase.php
+++ tests/class.tx_rnbase_tests_util_DB_testcase.php
@@ -29,15 +29,29 @@ tx_rnbase::load('tx_rnbase_util_DB');
 class tx_rnbase_tests_util_DB_testcase extends tx_phpunit_testcase {
 	function test_setSingleWhereField() {
 		$ret = tx_rnbase_util_DB::setSingleWhereField('Table1', OP_LIKE, 'Col1', 'myValue');
+		$this->assertEquals(" (Table1.col1 LIKE '%myValue%') ", $ret, 'LIKE failed.');
+
 //		self::debugString($ret);
-		$this->assertEquals(" (Table1.col1 LIKE '%myValue%')", $ret, 'LIKE failed.');
+		$ret = tx_rnbase_util_DB::setSingleWhereField('Table1', OP_INSET_INT, 'Col1', '23');
+		$this->assertEquals(" (FIND_IN_SET('23', Table1.col1)) ", $ret, 'FIND_IN_SET failed.');
+
+		$ret = tx_rnbase_util_DB::setSingleWhereField('Table1', OP_INSET_INT, 'Col1', '23,38');
+		$this->assertEquals(" (FIND_IN_SET('23', Table1.col1) OR FIND_IN_SET('38', Table1.col1)) ", $ret, 'FIND_IN_SET failed.');
 	}
 	
 	function test_searchWhere() {
 		$sw = 'content management, system';
 		$fields = 'tab1.bodytext,tab1.header';
+
+		$ret = tx_rnbase_util_DB::searchWhere('23', 'tab1.single', 'FIND_IN_SET_OR');
+		$this->assertEquals(" (FIND_IN_SET('23', tab1.single))", $ret, 'FIND_IN_SET failed.');
 		
+		$ret = tx_rnbase_util_DB::searchWhere('23', 't1.club,t2.club', OP_IN_INT);
+		$this->assertEquals(" (t1.club IN (23) OR t2.club IN (23) )", $ret, 'FIND_IN_SET failed.');
+		
+
 		$ret = tx_rnbase_util_DB::searchWhere($sw, $fields, OP_EQ);
+//		$this->debugString($ret);
 		$this->assertEquals($ret, " (tab1.bodytext = 'content' OR tab1.header = 'content' OR tab1.bodytext = 'management' OR tab1.header = 'management' OR tab1.bodytext = 'system' OR tab1.header = 'system' )", 'OR failed.');
 
 		$ret = tx_rnbase_util_DB::searchWhere($sw.', 32', $fields, 'FIND_IN_SET_OR');
