6e0a6bb77b09dce5836e47583ddfad0269c438a4
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2010-04-16 09:18:45 +0000

    Simple Logging-API

---
+++ tests/class.tx_rnbase_tests_Logger_testcase.php
@@ -0,0 +1,70 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Rene Nitzsche (rene@system25.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
+tx_rnbase::load('tx_rnbase_util_Logger');
+
+
+class tx_rnbase_tests_Logger_testcase extends tx_phpunit_testcase {
+
+	function test_logger() {
+		if(t3lib_extMgm::isLoaded('devlog')) {
+			$minLog = tx_rnbase_configurations::getExtensionCfgValue('devlog', 'minLogLevel');
+			if($minLog === false) {
+				$ret = tx_rnbase_util_Logger::isNoticeEnabled();
+				$this->assertTrue($ret, 'Notice funktioniert nicht.');
+			}
+			if($minLog == 1) {
+				$ret = tx_rnbase_util_Logger::isNoticeEnabled();
+				$this->assertTrue($ret, 'Notice funktioniert nicht.');
+				$ret = tx_rnbase_util_Logger::isWarningEnabled();
+				$this->assertTrue($ret, 'Warning funktioniert nicht.');
+				$ret = tx_rnbase_util_Logger::isFatalEnabled();
+				$this->assertTrue($ret, 'Fatal funktioniert nicht.');
+				$ret = tx_rnbase_util_Logger::isInfoEnabled();
+				$this->assertFalse($ret, 'Info funktioniert nicht.');
+				$ret = tx_rnbase_util_Logger::isDebugEnabled();
+				$this->assertFalse($ret, 'Debug funktioniert nicht.');
+			}
+			if($minLog == 3) {
+				$ret = tx_rnbase_util_Logger::isNoticeEnabled();
+				$this->assertFalse($ret, 'Notice funktioniert nicht.');
+				$ret = tx_rnbase_util_Logger::isWarningEnabled();
+				$this->assertFalse($ret, 'Warning funktioniert nicht.');
+				$ret = tx_rnbase_util_Logger::isFatalEnabled();
+				$this->assertTrue($ret, 'Fatal funktioniert nicht.');
+				$ret = tx_rnbase_util_Logger::isInfoEnabled();
+				$this->assertFalse($ret, 'Info funktioniert nicht.');
+				$ret = tx_rnbase_util_Logger::isDebugEnabled();
+				$this->assertFalse($ret, 'Debug funktioniert nicht.');
+			}
+		}
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/tests/class.tx_rnbase_tests_Logger_testcase.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/tests/class.tx_rnbase_tests_Logger_testcase.php']);
+}
+
+?>
---
+++ util/class.tx_rnbase_util_Logger.php
@@ -0,0 +1,133 @@
+<?php
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010 Rene Nitzsche
+ *  Contact: rene@system25.de
+ *  All rights reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ ***************************************************************/
+
+/**
+ * Default logger class.
+ */
+class tx_rnbase_util_Logger {
+	private static $minLog = false;
+
+	/**
+	 * Log a debug message
+	 * @param string $msg
+	 * @param string $extKey
+	 * @param mixed $dataVar
+	 */
+	public static function debug($msg, $extKey, $dataVar=FALSE) {
+		t3lib_div::devLog($msg, $extKey, -1, $dataVar);
+	}
+	/**
+	 * Log a notice
+	 * @param string $msg
+	 * @param string $extKey
+	 * @param mixed $dataVar
+	 */
+	public static function info($msg, $extKey, $dataVar=FALSE) {
+		t3lib_div::devLog($msg, $extKey, 0, $dataVar);
+	}
+	/**
+	 * Log a notice
+	 * @param string $msg
+	 * @param string $extKey
+	 * @param mixed $dataVar
+	 */
+	public static function notice($msg, $extKey, $dataVar=FALSE) {
+		t3lib_div::devLog($msg, $extKey, 1, $dataVar);
+	}
+	/**
+	 * Log a warning
+	 * @param string $msg
+	 * @param string $extKey
+	 * @param mixed $dataVar
+	 */
+	public static function warn($msg, $extKey, $dataVar=FALSE) {
+		t3lib_div::devLog($msg, $extKey, 2, $dataVar);
+	}
+	/**
+	 * Log a fatal error
+	 * @param string $msg
+	 * @param string $extKey
+	 * @param mixed $dataVar
+	 */
+	public static function fatal($msg, $extKey, $dataVar=FALSE) {
+		t3lib_div::devLog($msg, $extKey, 3, $dataVar);
+	}
+
+	/**
+	 * Whether or not log level notice is enabled.
+	 * This works only in conjunction with extension devlog
+	 * @return boolean
+	 */
+	public static function isDebugEnabled() {
+		return self::isLogLevel(-1);
+	}
+	/**
+	 * Whether or not log level notice is enabled.
+	 * This works only in conjunction with extension devlog
+	 * @return boolean
+	 */
+	public static function isInfoEnabled() {
+		return self::isLogLevel(0);
+	}
+	/**
+	 * Whether or not log level notice is enabled.
+	 * This works only in conjunction with extension devlog
+	 * @return boolean
+	 */
+	public static function isNoticeEnabled() {
+		return self::isLogLevel(1);
+	}
+	/**
+	 * Whether or not log level warning is enabled.
+	 * This works only in conjunction with extension devlog
+	 * @return boolean
+	 */
+	public static function isWarningEnabled() {
+		return self::isLogLevel(2);
+	}
+	/**
+	 * Whether or not log level fatal is enabled.
+	 * This works only in conjunction with extension devlog
+	 * @return boolean
+	 */
+	public static function isFatalEnabled() {
+		return self::isLogLevel(3);
+	}
+	private static function isLogLevel($level) {
+		if(self::$minLog === false) {
+			if(t3lib_extMgm::isLoaded('devlog')) {
+				$minLog = tx_rnbase_configurations::getExtensionCfgValue('devlog', 'minLogLevel');
+				self::$minLog = $minLog !== false ? $minLog : -1;
+			}
+		}
+		$isEnabled = $level >= self::$minLog;
+		return $isEnabled;
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/util/class.tx_rnbase_util_Logger.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/util/class.tx_rnbase_util_Logger.php']);
+}
+
+?>
