7ca21cc7db5265b4d270e105c6f406f0745d4185
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2010-05-01 12:58:04 +0000

    XClass fixed

--- util/class.tx_rnbase_util_SimpleMarker.php
+++ util/class.tx_rnbase_util_SimpleMarker.php
@@ -64,7 +64,7 @@ class tx_rnbase_util_SimpleMarker extends tx_rnbase_util_BaseMarker {
 }
 
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rnbase/util/class.tx_rnbase_util_SimpleMarker.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rnbase/util/class.tx_rnbase_util_SimpleMarker.php']);
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/util/class.tx_rnbase_util_SimpleMarker.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/util/class.tx_rnbase_util_SimpleMarker.php']);
 }
 ?>
