a30bc7c13980c6a9319d760b18ed54064cbdbbc5
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2010-05-03 15:06:49 +0000

    Include fixed

--- util/class.tx_rnbase_util_SimpleMarker.php
+++ util/class.tx_rnbase_util_SimpleMarker.php
@@ -22,9 +22,9 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-require_once(t3lib_extMgm::extPath('rn_base') . 'class.rn_base.php');
+require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
 
-rn_base::load('tx_rnbase_util_BaseMarker');
+tx_rnbase::load('tx_rnbase_util_BaseMarker');
 
 /**
  * A generic marker class. 
