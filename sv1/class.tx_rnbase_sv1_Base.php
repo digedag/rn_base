cbeede30b626fa25cf72ca16e8331ada28c3f26b
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2011-08-11 11:55:39 +0000

    Include von t3lib_svbase hinzugefügt.

--- sv1/class.tx_rnbase_sv1_Base.php
+++ sv1/class.tx_rnbase_sv1_Base.php
@@ -27,6 +27,7 @@
  * benötigte Klassen einbinden
  */
 require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
+require_once(PATH_t3lib.'class.t3lib_svbase.php');
 tx_rnbase::load('tx_rnbase_util_SearchBase');
 
 /**
