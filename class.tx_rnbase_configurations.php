464943686aadf82f1e5af130f07b9b80444907c1
digedag <digedag@204d2a1a-8740-0410-a1a2-ca9e6c71decb>
2011-11-05 13:23:10 +0000

    LANG-Abfrage für BE fehlte.

--- class.tx_rnbase_configurations.php
+++ class.tx_rnbase_configurations.php
@@ -328,8 +328,10 @@ class tx_rnbase_configurations {
 			// Get default translation (without charset conversion, english)
 			$word = $this->LOCAL_LANG['default'][$key][0]['target'];
 		} else {
-			// Return alternative string or empty
-			$word = (isset($this->LLtestPrefixAlt)) ? $this->LLtestPrefixAlt . $alternativeLabel : $alternativeLabel;
+			// Im BE die LANG fragen...
+			$word = is_object($GLOBALS['LANG']) ? $GLOBALS['LANG']->getLL($key) : '';
+			if(!$word)
+				$word = $this->LLtestPrefixAlt.$alt;
 		}
 	
 		$output = (isset($this->LLtestPrefix)) ? $this->LLtestPrefix . $word : $word;
@@ -355,7 +357,7 @@ class tx_rnbase_configurations {
 			// Im BE die LANG fragen...
 			$word = is_object($GLOBALS['LANG']) ? $GLOBALS['LANG']->getLL($key) : '';
 			if(!$word)
-			$word = $this->LLtestPrefixAlt.$alt;
+				$word = $this->LLtestPrefixAlt.$alt;
 		}
 	
 		$output = $this->LLtestPrefix.$word;
