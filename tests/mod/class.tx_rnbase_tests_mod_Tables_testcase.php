c45eac3acaae395d55fa7739a70461ab52b2b1d6
René Nitzsche <nitzsche@das-medienkombinat.de>
2012-07-20 11:43:24 +0200

    Unittest fixed

--- tests/mod/class.tx_rnbase_tests_mod_Tables_testcase.php
+++ tests/mod/class.tx_rnbase_tests_mod_Tables_testcase.php
@@ -1,26 +1,26 @@
 <?php
 /***************************************************************
  *  Copyright notice
- *
- *  (c) 2011 Hannes Bochmann (hannes.bochmann@das-medienkombinat.de)
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+*
+*  (c) 2011 Hannes Bochmann (hannes.bochmann@das-medienkombinat.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
 
 require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
 tx_rnbase::load('tx_rnbase_mod_Tables');
@@ -31,7 +31,12 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 	 * @var tx_rnbase_util_FormTool
 	 */
 	private $oFormTool;
-	
+	 
+	/**
+	 * @var string
+	 */
+	private $currentRequestUri;
+	 
 	/**
 	 * Initialisiert allgemeine Testdaten
 	 */
@@ -46,34 +51,62 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 			$GLOBALS['LOCAL_LANG']['default']['Header Uid'] = 'Header Uid';
 			$GLOBALS['LOCAL_LANG']['default']['Header Col1'] = 'Header Col1';
 		}
+		 
+		$this-> backupAndSetCurrentRequestUri();
+	}
+	 
+	/**
+	 */
+	protected function tearDown() {
+		$this->restoreCurrentRequestUri();
+	}
+	 
+	/**
+	 * Auf der CLI ist keine URL vorhanden und somit ist auch
+	 * nicht die normale BE URL vorhanden, welche für die Sortlinks benötigt wird.
+	 * Also setzen wir die URL einfach fest damit die Tests auch auf der CLI laufen.
+	 *
+	 * @return void
+	 */
+	private function backupAndSetCurrentRequestUri() {
+		$this->currentRequestUri =  $_SERVER['REQUEST_URI'];
+		$commonBeUrl = '/typo3/mod.php?M=tools_txphpunitbeM1';
+		$_SERVER['REQUEST_URI'] = $commonBeUrl;
+	}
+	 
+	/**
+	 * @return void
+	 */
+	private function restoreCurrentRequestUri() {
+		$_SERVER['REQUEST_URI'] = $this->currentRequestUri;
 	}
 	 
 	/**
 	 * Wird eine korrekte Tabelle zurückgeliefert wenn weder
-	 * ein decorator noch eine callback methode noch ein linker 
+	 * ein decorator noch eine callback methode noch ein linker
 	 * gesetzt ist und die Einträge als models vorliegen?
 	 */
 	public function testPrepareTableWithoutLinkerDecoratorOrCallbackMethodWithEntriesGivenAsModelsReturnsCorrectTable() {
 		$aEntries = array(
-			0 => tx_rnbase::makeInstance('tx_rnbase_model_base',array(
-				'uid' => 1,
-				'col1' => 'col1 Value 1'
-			)),
-			1 => tx_rnbase::makeInstance('tx_rnbase_model_base',array(
-				'uid' => 2,
-				'col1' => 'col1 Value 2'
-			)),
+				0 => tx_rnbase::makeInstance('tx_rnbase_model_base',array(
+						'uid' => 1,
+						'col1' => 'col1 Value 1'
+				)),
+				1 => tx_rnbase::makeInstance('tx_rnbase_model_base',array(
+						'uid' => 2,
+						'col1' => 'col1 Value 2'
+				)),
 		);
 		$aColumns = array(
-			'uid' => array(
-				'title' => 'Header Uid',
-			),
-			'col1' => array(
-				'title' => 'Header Col1',
-			)
+				'uid' => array(
+						'title' => 'Header Uid',
+				),
+				'col1' => array(
+						'title' => 'Header Col1',
+				)
 		);
 		$aRet = tx_rnbase_mod_Tables::prepareTable($aEntries, $aColumns, $this->oFormTool, array());
-		
+		 
 		//allgmein
 		$this->assertEquals(3,count($aRet[0]),'Das Array der gesamten Tabelle hat die falsche Anzahl an Elementen.');
 		//Header
@@ -89,33 +122,33 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 		$this->assertEquals(2,$aRet[0][2][0],'Die erste Zelle der zweiten Zeile ist falsch.');
 		$this->assertEquals('col1 Value 2',$aRet[0][2][1],'Die zweite Zelle der zweiten Zeile ist falsch.');
 	}
-	
+	 
 	/**
 	 * Wird eine korrekte Tabelle zurückgeliefert wenn weder
-	 * ein decorator noch eine callback methode noch ein linker 
+	 * ein decorator noch eine callback methode noch ein linker
 	 * gesetzt ist und die Einträge als array vorliegen?
 	 */
 	public function testPrepareTableWithoutLinkerDecoratorOrCallbackMethodWithEntriesGivenAsArraysReturnsCorrectTable() {
 		$aEntries = array(
-			0 => array(
-				'uid' => 1,
-				'col1' => 'col1 Value 1'
-			),
-			1 => array(
-				'uid' => 2,
-				'col1' => 'col1 Value 2'
-			),
+				0 => array(
+						'uid' => 1,
+						'col1' => 'col1 Value 1'
+				),
+				1 => array(
+						'uid' => 2,
+						'col1' => 'col1 Value 2'
+				),
 		);
 		$aColumns = array(
-			'uid' => array(
-				'title' => 'Header Uid',
-			),
-			'col1' => array(
-				'title' => 'Header Col1',
-			)
+				'uid' => array(
+						'title' => 'Header Uid',
+				),
+				'col1' => array(
+						'title' => 'Header Col1',
+				)
 		);
 		$aRet = tx_rnbase_mod_Tables::prepareTable($aEntries, $aColumns, $this->oFormTool, array());
-		
+		 
 		//allgmein
 		$this->assertEquals(3,count($aRet[0]),'Das Array der gesamten Tabelle hat die falsche Anzahl an Elementen.');
 		//Header
@@ -131,38 +164,38 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 		$this->assertEquals(2,$aRet[0][2][0],'Die erste Zelle der zweiten Zeile ist falsch.');
 		$this->assertEquals('col1 Value 2',$aRet[0][2][1],'Die zweite Zelle der zweiten Zeile ist falsch.');
 	}
-	
+	 
 	/**
 	 * Wird eine korrekte Tabelle zurückgeliefert wenn weder
-	 * eine callback methode noch ein linker gesetzt ist 
+	 * eine callback methode noch ein linker gesetzt ist
 	 * aber ein decorator und die Einträge als models vorliegen?
 	 */
 	public function testPrepareTableWithoutLinkerOrCallbackMethodWithDecoratorAndEntriesGivenAsModelsReturnsCorrectTable() {
 		$oDecorator = tx_rnbase::makeInstance('tx_rnbase_tests_fixtures_classes_Decorator',
-			tx_rnbase::makeInstance('tx_rnbase_tests_fixtures_classes_Mod')
+				tx_rnbase::makeInstance('tx_rnbase_tests_fixtures_classes_Mod')
 		);
 		$aEntries = array(
-			0 => tx_rnbase::makeInstance('tx_rnbase_model_base',array(
-				'uid' => 1,
-				'col1' => 'col1 Value 1'
-			)),
-			1 => tx_rnbase::makeInstance('tx_rnbase_model_base',array(
-				'uid' => 2,
-				'col1' => 'col1 Value 2'
-			)),
+				0 => tx_rnbase::makeInstance('tx_rnbase_model_base',array(
+						'uid' => 1,
+						'col1' => 'col1 Value 1'
+				)),
+				1 => tx_rnbase::makeInstance('tx_rnbase_model_base',array(
+						'uid' => 2,
+						'col1' => 'col1 Value 2'
+				)),
 		);
 		$aColumns = array(
-			'uid' => array(
-				'title' => 'Header Uid',
-				'decorator' => &$oDecorator,
-			),
-			'col1' => array(
-				'title' => 'Header Col1',
-				'decorator' => &$oDecorator,
-			)
+				'uid' => array(
+						'title' => 'Header Uid',
+						'decorator' => &$oDecorator,
+				),
+				'col1' => array(
+						'title' => 'Header Col1',
+						'decorator' => &$oDecorator,
+				)
 		);
 		$aRet = tx_rnbase_mod_Tables::prepareTable($aEntries, $aColumns, $this->oFormTool, array());
-		
+		 
 		//allgmein
 		$this->assertEquals(3,count($aRet[0]),'Das Array der gesamten Tabelle hat die falsche Anzahl an Elementen.');
 		//Header
@@ -178,29 +211,29 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 		$this->assertEquals(2,$aRet[0][2][0],'Die erste Zelle der zweiten Zeile ist falsch.');
 		$this->assertEquals('spalte1 Value 2',$aRet[0][2][1],'Die zweite Zelle der zweiten Zeile ist falsch.');
 	}
-	
+	 
 	/**
 	 * Wird eine korrekte Tabelle zurückgeliefert wenn noch keine Sortierung gewählt wurde?
 	 */
 	public function testPrepareTableWithSortableButNothingSelectedReturnsCorrectTable() {
 		$aEntries = array(
-			0 => array(
-				'uid' => 1,
-				'col1' => 'col1 Value 1'
-			),
-			1 => array(
-				'uid' => 2,
-				'col1' => 'col1 Value 2'
-			),
+				0 => array(
+						'uid' => 1,
+						'col1' => 'col1 Value 1'
+				),
+				1 => array(
+						'uid' => 2,
+						'col1' => 'col1 Value 2'
+				),
 		);
 		$aColumns = array(
 			'uid' => array(
-				'title' => 'Header Uid',
-				'sortable' => 'TestPrefix.',
-			),
-			'col1' => array(
-				'title' => 'Header Col1',
-			)
+						'title' => 'Header Uid',
+						'sortable' => 'TestPrefix.',
+				),
+				'col1' => array(
+						'title' => 'Header Col1',
+				)
 		);
 		$aRet = tx_rnbase_mod_Tables::prepareTable($aEntries, $aColumns, $this->oFormTool, array());
 		//allgmein
@@ -217,39 +250,42 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 		$this->assertEquals(2,count($aRet[0][2]),'Das Array der zweiten Zeile hat die falsche Anzahl an Elementen.');
 		$this->assertEquals(2,$aRet[0][2][0],'Die erste Zelle der zweiten Zeile ist falsch.');
 		$this->assertEquals('col1 Value 2',$aRet[0][2][1],'Die zweite Zelle der zweiten Zeile ist falsch.');
+		 
+		$_SERVER['REQUEST_URI'] = $currentUriBackup;
 	}
-	
+	 
 	/**
 	 * Wird eine korrekte Tabelle zurückgeliefert wenn noch keine Sortierung gewählt wurde?
 	 */
 	public function testPrepareTableWithSortableChangesSortingCorrect() {
 		$_GET['sortField'] = 'uid';
 		$aEntries = array(
-			0 => array(
-				'uid' => 1,
-				'col1' => 'col1 Value 1'
-			),
-			1 => array(
-				'uid' => 2,
-				'col1' => 'col1 Value 2'
-			),
+				0 => array(
+						'uid' => 1,
+						'col1' => 'col1 Value 1'
+				),
+				1 => array(
+						'uid' => 2,
+						'col1' => 'col1 Value 2'
+				),
 		);
 		$aColumns = array(
-			'uid' => array(
-				'title' => 'Header Uid',
-				'sortable' => 'TestPrefix.',
-			),
-			'col1' => array(
-				'title' => 'Header Col1',
-			)
+				'uid' => array(
+						'title' => 'Header Uid',
+						'sortable' => 'TestPrefix.',
+				),
+				'col1' => array(
+						'title' => 'Header Col1',
+				)
 		);
 		$aRet = tx_rnbase_mod_Tables::prepareTable($aEntries, $aColumns, $this->oFormTool, array());
-		
+		 
 		//allgmein
 		$this->assertEquals(3,count($aRet[0]),'Das Array der gesamten Tabelle hat die falsche Anzahl an Elementen.');
 		//Header
 		$this->assertEquals(2,count($aRet[0][0]),'Das Array des Headers hat die falsche Anzahl an Elementen.');
 		$this->assertContains('&amp;sortField=uid&amp;sortRev=desc">Header Uid',$aRet[0][0][0],'Die erste Zelle des Headers ist falsch. 1. teil');
+
 		//der korrekte pfeil?
 		$this->assertContains('/icons/gfx/reddown.gif',$aRet[0][0][0],'Die erste Zelle des Headers ist falsch. 2. teil');
 		$this->assertEquals('Header Col1',$aRet[0][0][1],'Die zweite Zelle des Headers ist falsch.');
@@ -262,7 +298,7 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 		$this->assertEquals(2,$aRet[0][2][0],'Die erste Zelle der zweiten Zeile ist falsch.');
 		$this->assertEquals('col1 Value 2',$aRet[0][2][1],'Die zweite Zelle der zweiten Zeile ist falsch.');
 	}
-	
+	 
 	/**
 	 * Wird eine korrekte Tabelle zurückgeliefert wenn noch keine Sortierung gewählt wurde?
 	 */
@@ -270,26 +306,26 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 		$_GET['sortField'] = 'uid';
 		$_GET['sortRev'] = 'desc';
 		$aEntries = array(
-			0 => array(
-				'uid' => 1,
-				'col1' => 'col1 Value 1'
-			),
-			1 => array(
-				'uid' => 2,
-				'col1' => 'col1 Value 2'
-			),
+				0 => array(
+						'uid' => 1,
+						'col1' => 'col1 Value 1'
+				),
+				1 => array(
+						'uid' => 2,
+						'col1' => 'col1 Value 2'
+				),
 		);
 		$aColumns = array(
-			'uid' => array(
-				'title' => 'Header Uid',
-				'sortable' => 'TestPrefix.',
-			),
-			'col1' => array(
-				'title' => 'Header Col1',
-			)
+				'uid' => array(
+						'title' => 'Header Uid',
+						'sortable' => 'TestPrefix.',
+				),
+				'col1' => array(
+						'title' => 'Header Col1',
+				)
 		);
 		$aRet = tx_rnbase_mod_Tables::prepareTable($aEntries, $aColumns, $this->oFormTool, array());
-		
+		 
 		//allgmein
 		$this->assertEquals(3,count($aRet[0]),'Das Array der gesamten Tabelle hat die falsche Anzahl an Elementen.');
 		//Header
@@ -307,7 +343,7 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 		$this->assertEquals(2,$aRet[0][2][0],'Die erste Zelle der zweiten Zeile ist falsch.');
 		$this->assertEquals('col1 Value 2',$aRet[0][2][1],'Die zweite Zelle der zweiten Zeile ist falsch.');
 	}
-	
+	 
 	/**
 	 * Wird eine korrekte Tabelle zurückgeliefert wenn noch keine Sortierung gewählt wurde?
 	 */
@@ -315,28 +351,28 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 		$_GET['sortField'] = 'uid';
 		//weiterer Param, der erhalten bleiben sollte
 		$_SERVER['REQUEST_URI'] = $_SERVER['REQUEST_URI'].'&sortField=title&sortRev=desc&additionalParam=test';
-		
+		 
 		$aEntries = array(
-			0 => array(
-				'uid' => 1,
-				'col1' => 'col1 Value 1'
-			),
-			1 => array(
-				'uid' => 2,
-				'col1' => 'col1 Value 2'
-			),
+				0 => array(
+						'uid' => 1,
+						'col1' => 'col1 Value 1'
+				),
+				1 => array(
+						'uid' => 2,
+						'col1' => 'col1 Value 2'
+				),
 		);
 		$aColumns = array(
-			'uid' => array(
-				'title' => 'Header Uid',
-				'sortable' => 'TestPrefix.',
-			),
-			'col1' => array(
-				'title' => 'Header Col1',
-			)
+				'uid' => array(
+						'title' => 'Header Uid',
+						'sortable' => 'TestPrefix.',
+				),
+				'col1' => array(
+						'title' => 'Header Col1',
+				)
 		);
 		$aRet = tx_rnbase_mod_Tables::prepareTable($aEntries, $aColumns, $this->oFormTool, array());
-		
+		 
 		//allgmein
 		$this->assertEquals(3,count($aRet[0]),'Das Array der gesamten Tabelle hat die falsche Anzahl an Elementen.');
 		//Header
@@ -360,4 +396,3 @@ class tx_rnbase_tests_mod_Tables_testcase extends tx_phpunit_testcase {
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/tests/mod/class.tx_rnbase_tests_mod_Tables_testcase.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/tests/mod/class.tx_rnbase_tests_mod_Tables_testcase.php']);
 }
-
