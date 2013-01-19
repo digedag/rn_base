495f0268f66efa332e32946db9e51773d6b5e910
digedag <rene@system25.de>
2013-01-19 18:14:39 +0100

    new class util_Math

---
+++ util/class.tx_rnbase_util_Math.php
@@ -0,0 +1,46 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Rene Nitzsche (rene@system25.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+/**
+ * Wrapper for math usage.
+ */
+class tx_rnbase_util_Math {
+	/**
+	 * Tests if the input can be interpreted as integer.
+	 * @return boolean
+	 */
+	public static function testInt($var) {
+		if(tx_rnbase_util_TYPO3::isTYPO46OrHigher()) {
+			return t3lib_utility_Math::canBeInterpretedAsInteger($var);
+		}
+		else {
+			return t3lib_div::testInt($var);
+		}
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/util/class.tx_rnbase_util_Math.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/util/class.tx_rnbase_util_Math.php']);
+}
