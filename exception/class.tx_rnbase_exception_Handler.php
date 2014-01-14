e9e99f3d7e17c0c01f0ebce1906e846527c43248
Michael Wagner <michael.wagner@das-medienkombinat.de>
2014-01-14 11:37:17 +0100

    catch exceptions by typoscript in exeptionhandler

--- exception/class.tx_rnbase_exception_Handler.php
+++ exception/class.tx_rnbase_exception_Handler.php
@@ -27,23 +27,33 @@ tx_rnbase::load('tx_rnbase_exception_IHandler');
 
 /**
  * @author Hannes Bochmann
+ * @author Michael Wagner
  *
  */
 class tx_rnbase_exception_Handler implements tx_rnbase_exception_IHandler {
 
   	/**
 	 * Interne Verarbeitung der Exception
-	 * 
+	 *
 	 * @param string $actionName
 	 * @param Exception $e
 	 * @param tx_rnbase_configurations $configurations
-	 * 
+	 *
 	 * @return string error message
 	 */
 	public function handleException($actionName, Exception $e, tx_rnbase_configurations $configurations) {
+
+		// wir prüfen erst mal, ob die exception gefangen werden soll
+		$catch = $this->catchException($actionName, $e, $configurations);
+		if ($catch !== NULL) {
+			return $catch;
+		}
+
+		// wenn nicht senden wir ggf den header
 		if($this->send503HeaderOnException($configurations)) {
 			header('HTTP/1.1 503 Service Unavailable');
 		}
+		// wir loggen nun den fehler
 		tx_rnbase::load('tx_rnbase_util_Logger');
 		if(tx_rnbase_util_Logger::isFatalEnabled()) {
 			$extKey = $configurations->getExtensionKey();
@@ -51,6 +61,7 @@ class tx_rnbase_exception_Handler implements tx_rnbase_exception_IHandler {
 			tx_rnbase_util_Logger::fatal('Fatal error for action ' . $actionName, $extKey,
 				array('Exception'=> $e, '_GET' => $_GET, '_POST' => $_POST));
 		}
+		// wir senden eine fehlermail
 		$addr = tx_rnbase_configurations::getExtensionCfgValue('rn_base', 'sendEmailOnException');
 		if($addr) {
 			tx_rnbase_util_Misc::sendErrorMail($addr, $actionName, $e);
@@ -60,11 +71,11 @@ class tx_rnbase_exception_Handler implements tx_rnbase_exception_IHandler {
 		$ret = $this->getErrorMessage($actionName, $e, $configurations);
 		return $ret;
 	}
- 
+
 	/**
-	 * 
+	 *
 	 * @param tx_rnbase_configurations $configurations
-	 * 
+	 *
 	 * @return void
 	 */
 	protected function send503HeaderOnException(tx_rnbase_configurations $configurations) {
@@ -85,26 +96,146 @@ class tx_rnbase_exception_Handler implements tx_rnbase_exception_IHandler {
 			)
 		);
 	}
-	
+
 	/**
 	 * Build an error message string for frontend
 	 * @param string $actionName
 	 * @param Exception $e
 	 * @param tx_rnbase_Configurations $configurations
 	 */
-	protected function getErrorMessage($actionName, Exception $e, $configurations) {
-		$verbose = intval(tx_rnbase_configurations::getExtensionCfgValue('rn_base', 'verboseMayday')) ?
-		'<br /><pre>'.$e->__toString().'</pre>' : '';
-	
-		$ret = $configurations->getLL('ERROR_'.$e->getCode(), '');
+	protected function getErrorMessage($actionName, Exception $e, tx_rnbase_configurations $configurations) {
+		if (tx_rnbase_configurations::getExtensionCfgValue('rn_base', 'verboseMayday')) {
+			return '<div>'
+					. '<strong>UNCAUGHT EXCEPTION FOR VIEW: ' . $actionName . '</strong>'
+					. '<br />CODE: ' . $e->getCode()
+					. '<br />MESSAGE: ' . $e->getMessage()
+					. '<br />STACK: <pre>' . $e->__toString() . '</pre>'
+				. '</div>';
+		}
+
+		// typoscript nach fehlermeldungen prüfen
+		// das machen wir nur, wenn sich diese exception nicht
+		// dies kann passieren, wenn über typoscript wieder plugin angesteuert wird,
+		// welches die selbe fehlermeldung produziert
+		if (!$this->checkExceptionRecursion($actionName, $e, $configurations)) {
+			// aktuelle meldung setzen, damit diese ggf. über ts ausgelesen werden kann (.current = 1)
+			$configurations->getCObj()->setCurrentVal('ERROR '.$e->getCode().': '.$e->getMessage());
+			// meldung auslesen
+			$ret = $configurations->get('error.'.$e->getCode(), true);
+			$ret = $ret ? $ret : $configurations->get('error.default', true);
+		}
+		// nun die sprachlabels nach einem fehler überprüfen.
+		$ret = isset($ret) && $ret ? $ret : $configurations->getLL('ERROR_'.$e->getCode(), '');
 		$ret = $ret ? $ret : $configurations->getLL('ERROR_default', '');
-		if($verbose) {
-			$ret = '<div><strong>UNCAUGHT EXCEPTION FOR VIEW: ' . $actionName .'</strong>'.$verbose.'</div>';
-		}
-		elseif(!$ret) {
-			$ret = '<div><strong>Leider ist ein unerwarteter Fehler aufgetreten.</strong></div>';
-		}
+		// fallback message
+		$ret = $ret ? $ret : '<div><strong>Leider ist ein unerwarteter Fehler aufgetreten.</strong></div>';
 		return $ret;
+	}
+
+	/**
+	 * prüft ob die exception gefangen werden kann,
+	 * und erzeugt ggf eine alternative ausgabe.
+	 *
+	 * liefert entweder eine ausgabe (string) oder null,
+	 * um die fehlerbehandlung weiter leufen zu lassen.
+	 *
+	 * @param string $actionName
+	 * @param Exception $e
+	 * @param tx_rnbase_configurations $configurations
+	 * @return string|null
+	 */
+	protected function catchException(
+		$actionName,
+		Exception $e,
+		tx_rnbase_configurations $configurations
+	) {
+		// typoscript nach catchanweisungen prüfen
+		// das machen wir nur, wenn sich diese exception nicht
+		// dies kann passieren, wenn über typoscript wieder plugin angesteuert wird,
+		// welches die selbe fehlermeldung produziert
+		if (!$this->checkExceptionRecursion($actionName, $e, $configurations, 'catch')) {
+			// aktuelle meldung setzen, damit diese ggf. über ts ausgelesen werden kann (.current = 1)
+			$configurations->getCObj()->setCurrentVal('ERROR '.$e->getCode().': '.$e->getMessage());
+			// meldung auslesen
+			$ret = $configurations->get('catchException.' . $e->getCode(), true);
+			if (!empty($ret)) {
+				return $ret;
+			}
+		}
+		return NULL;
+	}
+
+	/**
+	 * chechs, if the exception was allready thrown in the stack.
+	 * returns true, if the exception was allready thrown.
+	 *
+	 * die recursion können wir an dieser stelle nicht über den backtrace prüfen.
+	 * bei ungecachten ausgaben wird bei typo3 mit int_script gearbeidet,
+	 * wodurch der stack auch bei mehrfacher rekursion immer gleich ist,
+	 * also die methode nur ein mal auftaucht.
+	 *
+	 * @param string $actionName
+	 * @param Exception $e
+	 * @param tx_rnbase_configurations $configurations
+	 * @return boolean
+	 */
+	private function checkExceptionRecursion(
+		$action,
+		Exception $e,
+		tx_rnbase_configurations $configurations,
+		$type = 'error'
+	) {
+		static $calls = 0, $trace = array();
+
+		// konfiguration für die maximale anzahl an durchläufen holen.
+		$maxCalls = $configurations->getInt('recursionCheck.maxCalls');
+		$maxCalls = $maxCalls ? $maxCalls : 50;
+		$maxThrows = $configurations->getInt('recursionCheck.maxThrows');
+		$maxThrows = $maxThrows ? $maxThrows : 1;
+
+		// bei mehr als 50 exception calls, müssen wir davon ausgehen,
+		// das ein kritischer fehler vorliegt
+		if (++$calls > $maxCalls) {
+			tx_rnbase_util_Logger::fatal(
+				'Too much recursion in "'.get_class($this).'"'
+					. ' That should not have happened.'
+					. ' It looks as if there is a problem with a faulty configuration.',
+				'rn_base'
+			);
+			return true;
+		}
+		// else
+		// ansonsten setzen wir eine art stack aus action, errorcode und config zusammen.
+
+		$code = $e->getCode();
+		// das typoscript wir bei jedem plugin aufruf neu generiert
+		// und unterscheidet sich demnach.
+		// wenn es zu einer rekursion kommt, ist das ts allerdings immer gleich!
+		// (abgesehen von unaufgelösten referenzen)
+		$configKey = md5(serialize($configurations->getConfigArray()));
+
+		if (empty($trace[$type])) {
+			$trace[$type] = array();
+		}
+		if (empty($trace[$type][$action])) {
+			$trace[$type][$action] = array();
+		}
+		if (empty($trace[$type][$action][$code])) {
+			$trace[$type][$action][$code] = array();
+		}
+		if (empty($trace[$type][$action][$code][$configKey])) {
+			$trace[$type][$action][$code][$configKey] = 0;
+		}
+		$trace[$type][$action][$code][$configKey]++;
+
+		if (
+			isset($trace[$type][$action][$code][$configKey])
+			&& $trace[$type][$action][$code][$configKey] > $maxThrows
+		) {
+			return true;
+		}
+
+		return false;
 	}
 }
 
