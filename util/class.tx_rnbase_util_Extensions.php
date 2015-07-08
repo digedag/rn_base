162d7a1b4acae766485ebfe8a69f69eb3ff53ef2
René Nitzsche <rene@system25.de>
2015-07-08 11:48:26 +0200

    Wrapperclass for ExtensionManager

---
+++ util/class.tx_rnbase_util_Extensions.php
@@ -0,0 +1,52 @@
+<?php
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Rene Nitzsche
+ *  Contact: rene@system25.de
+ *  All rights reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ ***************************************************************/
+
+/**
+ * Wrapperclass for TYPO3 Extension Manager
+ * @author René Nitzsche
+ *
+ */
+class tx_rnbase_util_Extensions {
+
+	/**
+	 * Wrapper for t3lib_extMgm::addStaticFile
+	 *
+	 * Call this method to add an entry in the static template list found in sys_templates
+	 * "static template files" are the modern equalent (provided from extensions) to the traditional records in "static_templates"
+	 * FOR USE IN ext_localconf.php FILES
+	 * Usage: 3
+	 *
+	 * @param	string		$extKey is of course the extension key
+	 * @param	string		$path is the path where the template files (fixed names) include_static.txt (integer list of uids from the table "static_templates"), constants.txt, setup.txt, editorcfg.txt, and include_static_file.txt is found (relative to extPath, eg. 'static/'). The file include_static_file.txt, allows you to include other static templates defined in files, from your static template, and thus corresponds to the field 'include_static_file' in the sys_template table. The syntax for this is a commaseperated list of static templates to include, like:  EXT:css_styled_content/static/,EXT:da_newsletter_subscription/static/,EXT:cc_random_image/pi2/static/
+	 * @param	string		$title is the title in the selector box.
+	 * @return	void
+	 * @see addTypoScript()
+	 */
+	public static function addStaticFile($extKey, $path, $title)	{
+		if(tx_rnbase_util_TYPO3::isTYPO60OrHigher())
+			return \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, $path, $title);
+		return t3lib_extMgm::addStaticFile($extKey, $path, $title);
+	}
+
+}
