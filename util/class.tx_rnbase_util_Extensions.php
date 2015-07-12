ac0c22a65faa4572ad4b822a94fb46d454844ea9
René Nitzsche <rene@system25.de>
2015-07-12 20:33:41 +0200

    New methods to extend TCA

--- util/class.tx_rnbase_util_Extensions.php
+++ util/class.tx_rnbase_util_Extensions.php
@@ -90,4 +90,41 @@ class tx_rnbase_util_Extensions {
 			return \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded($key, $exitOnError);
 		return t3lib_extMgm::isLoaded($key, $exitOnError);
 	}
+
+	/**
+	 * Adding fields to an existing table definition in $GLOBALS['TCA']
+	 * Adds an array with $GLOBALS['TCA'] column-configuration to the $GLOBALS['TCA']-entry for that table.
+	 * This function adds the configuration needed for rendering of the field in TCEFORMS - but it does NOT add the field names to the types lists!
+	 * So to have the fields displayed you must also call fx. addToAllTCAtypes or manually add the fields to the types list.
+	 * FOR USE IN ext_tables.php FILES or files in Configuration/TCA/Overrides/*.php Use the latter to benefit from TCA caching!
+	 *
+	 * @param string $table The table name of a table already present in $GLOBALS['TCA'] with a columns section
+	 * @param array $columnArray The array with the additional columns (typical some fields an extension wants to add)
+	 * @param boolean $addTofeInterface DEPRECATED: Usage of feInterface is no longer part of the TYPO3 CMS Core. Please check EXT:statictemplates.
+	 * @return void
+	 */
+	public static function addTCAcolumns($table, $columnArray, $addTofeInterface = FALSE) {
+		if(tx_rnbase_util_TYPO3::isTYPO60OrHigher())
+			return \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $columnArray, $addTofeInterface);
+		return t3lib_extMgm::addTCAcolumns($table, $columnArray, $addTofeInterface);
+	}
+	/**
+	 * Makes fields visible in the TCEforms, adding them to the end of (all) "types"-configurations
+	 *
+	 * Adds a string $string (comma separated list of field names) to all ["types"][xxx]["showitem"] entries for table $table (unless limited by $typeList)
+	 * This is needed to have new fields shown automatically in the TCEFORMS of a record from $table.
+	 * Typically this function is called after having added new columns (database fields) with the addTCAcolumns function
+	 * FOR USE IN ext_tables.php FILES or files in Configuration/TCA/Overrides/*.php Use the latter to benefit from TCA caching!
+	 *
+	 * @param string $table Table name
+	 * @param string $newFieldsString Field list to add.
+	 * @param string $typeList List of specific types to add the field list to. (If empty, all type entries are affected)
+	 * @param string $position Insert fields before (default) or after one, or replace a field
+	 * @return void
+	 */
+	public static function addToAllTCAtypes($table, $newFieldsString, $typeList = '', $position = '') {
+		if(tx_rnbase_util_TYPO3::isTYPO60OrHigher())
+			return \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, $newFieldsString, $typeList, $position);
+		return t3lib_extMgm::addToAllTCAtypes($table, $newFieldsString, $typeList, $position);
+	}
 }
