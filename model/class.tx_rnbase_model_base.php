b77e6868e52d0a0292b3e4156434ef283ef49da4
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2016-01-19 14:40:32 +0100

    Revert "dependency for old to new models !?"
    This reverts commit e0d5d78203a66a80d14754fda22b925b4d2114d0.

--- model/class.tx_rnbase_model_base.php
+++ model/class.tx_rnbase_model_base.php
@@ -22,7 +22,13 @@
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-tx_rnbase::load('Tx_Rnbase_Domain_Model_Base');
+tx_rnbase::load('tx_rnbase_model_data');
+tx_rnbase::load('Tx_Rnbase_Domain_Model_RecordInterface');
+tx_rnbase::load('Tx_Rnbase_Domain_Model_DomainInterface');
+tx_rnbase::load('tx_rnbase_util_TCA');
+// Die Datenbank-Klasse
+tx_rnbase::load('tx_rnbase_util_DB');
+
 
 /**
  * Basisklasse für die meisten Model-Klassen. Sie stellt einen Konstruktor bereit, der sowohl
@@ -35,23 +41,295 @@ tx_rnbase::load('Tx_Rnbase_Domain_Model_Base');
  *              THIS CLASS WILL BE DROPPED IN THE FUTURE!!!
  */
 class tx_rnbase_model_base
-	extends Tx_Rnbase_Domain_Model_Base
+	extends tx_rnbase_model_data
+		implements Tx_Rnbase_Domain_Model_RecordInterface, Tx_Rnbase_Domain_Model_DomainInterface
 {
-	public function tx_rnbase_model_base($record = NULL) {
-		return $this->init($record);
+
+	var $uid;
+
+	/**
+	 *
+	 * @var string|0
+	 */
+	private $tableName = 0;
+
+	function tx_rnbase_model_base($rowOrUid = NULL) {
+		return $this->init($rowOrUid);
 	}
 
 	/**
-	 * @var int $uid
+	 * Most model-classes will be initialized by a uid or a database record. So
+	 * this is a common contructor.
+	 * Ensure to overwrite getTableName()!
+	 *
+	 * @param mixed $rowOrUid
+	 * @return NULL
 	 */
-	public $uid;
+	function __construct($rowOrUid = NULL) {
+		return $this->tx_rnbase_model_base($rowOrUid);
+	}
 
 	/**
-	 * holds the data!
+	 * Inits the model instance either with uid or a complete data record.
+	 * As the result the instance should be completly loaded.
 	 *
-	 * @var array
+	 * @param mixed $rowOrUid
+	 * @return NULL
 	 */
-	public $record = array();
+	function init($rowOrUid = NULL) {
+		if (is_array($rowOrUid)) {
+			$this->uid = $rowOrUid['uid'];
+			$this->record = $rowOrUid;
+		}
+		else {
+			$rowOrUid = (int) $rowOrUid;
+			$this->uid = $rowOrUid;
+			if ($rowOrUid === 0) {
+				$this->record = array();
+			} elseif($this->getTableName()) {
+				$options = array();
+				if(is_object($GLOBALS['BE_USER']) && $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['rn_base']['loadHiddenObjects']) {
+					$options['enablefieldsbe'] = 1;
+				}
+				$this->record = tx_rnbase_util_DB::getRecord($this->getTableName(), $this->uid, $options);
+			}
+			// Der Record sollte immer ein Array sein
+			$this->record = is_array($this->record) ? $this->record : array();
+		}
+
+		// set the modified state to clean
+		$this->resetCleanState();
+
+		return NULL;
+	}
+	/**
+	 * Returns the records uid
+	 *
+	 * @return int
+	 */
+	function getUid() {
+		$uid = 0;
+		$tableName = $this->getTableName();
+		if (!empty($tableName)) {
+			// Take care for localized records where uid of original record
+			// is stored in $this->record['l18n_parent'] instead of $this->record['uid']!
+			$languageParentField = tx_rnbase_util_TCA::getTransOrigPointerFieldForTable($tableName);
+			$sysLanguageUidField = tx_rnbase_util_TCA::getLanguageFieldForTable($tableName);
+			if (
+				!(
+					empty($languageParentField)
+					&& empty($sysLanguageUidField)
+					&& empty($this->record[$sysLanguageUidField])
+					&& empty($this->record[$languageParentField])
+				)
+			) {
+				$uid = $this->record[$languageParentField];
+			}
+		}
+		return $uid > 0 ? $uid : $this->uid;
+	}
+
+	/**
+	 * Returns the label of the record, defined in the tca.
+	 *
+	 * @return int
+	 */
+	public function getTcaLabel() {
+		$label = '';
+		$tableName = $this->getTableName();
+		if (!empty($tableName)) {
+			$labelField = tx_rnbase_util_TCA::getLabelFieldForTable($tableName);
+			if (!$this->isPropertyEmpty($labelField)) {
+				$label = (string) $this->getProperty($labelField);
+			}
+		}
+		return $label;
+	}
+	/**
+	 * Returns the Language id of the record.
+	 *
+	 * @return int
+	 */
+	public function getSysLanguageUid() {
+		$uid = 0;
+		$tableName = $this->getTableName();
+		if (!empty($tableName)) {
+			$sysLanguageUidField = tx_rnbase_util_TCA::getLanguageFieldForTable($tableName);
+			if (!$this->isPropertyEmpty($sysLanguageUidField)) {
+				$uid = (int) $this->getProperty($sysLanguageUidField);
+			}
+		}
+		return $uid;
+	}
+
+	/**
+	 * Returns the creation date of the record as DateTime object.
+	 *
+	 * @param DateTimeZone $timezone
+	 * @return DateTime
+	 */
+	public function getCreationDateTime($timezone=NULL) {
+		$datetime = NULL;
+		$tableName = $this->getTableName();
+		if (!empty($tableName)) {
+			$field = tx_rnbase_util_TCA::getCrdateFieldForTable($tableName);
+			if (!$this->isPropertyEmpty($field)) {
+				$tstamp = (int) $this->getProperty($field);
+				tx_rnbase::load('tx_rnbase_util_Dates');
+				$datetime = tx_rnbase_util_Dates::getDateTime('@' . $tstamp);
+			}
+		}
+		return $datetime;
+	}
+
+	/**
+	 * Returns the creation date of the record as DateTime object.
+	 *
+	 * @return DateTime
+	 */
+	public function getLastModifyDateTime($timezone=NULL) {
+		$datetime = NULL;
+		$tableName = $this->getTableName();
+		if (!empty($tableName)) {
+			$field = tx_rnbase_util_TCA::getTstampFieldForTable($tableName);
+			if (!$this->isPropertyEmpty($field)) {
+				$tstamp = (int) $this->getProperty($field);
+				tx_rnbase::load('tx_rnbase_util_Dates');
+				$datetime = tx_rnbase_util_Dates::getDateTime('@' . $tstamp);
+			}
+		}
+		return $datetime;
+	}
+
+	/**
+	 * Reload this records from database
+	 *
+	 * @return tx_rnbase_model_base
+	 */
+	function reset() {
+		$this->record = tx_rnbase_util_DB::getRecord(
+			$this->getTableName(),
+			$this->getUid()
+		);
+
+		// set the modified state to clean
+		$this->resetCleanState();
+
+		return $this;
+	}
+	/**
+	 * Liefert den aktuellen Tabellenname
+	 *
+	 * @return Tabellenname als String
+	 */
+	public function getTableName() {
+		return $this->tableName;
+	}
+
+	/**
+	 * Setzt den aktuellen Tabellenname
+	 *
+	 * @param string $tableName
+	 * @return tx_rnbase_model_base
+	 */
+	public function setTableName($tableName = 0) {
+		$this->tableName = $tableName;
+		return $this;
+	}
+
+	/**
+	 * Check if this record is valid. If FALSE, the record is maybe deleted in database.
+	 *
+	 * @return boolean
+	 */
+	public function isValid() {
+		return count($this->record) > 0;
+	}
+
+	/**
+	 * Check if record is persisted in database. This is if uid is not 0.
+	 *
+	 * @return boolean
+	 */
+	public function isPersisted() {
+		return $this->getUid() > 0;
+	}
+
+	/**
+	 * validates the data of a model with the tca definition of a its table.
+	 *
+	 * @param array $options
+	 *     only_record_fields: validates only fields included in the record (default)
+	 * @return bolean
+	 */
+	public function validateProperties($options = NULL) {
+		return tx_rnbase_util_TCA::validateModel(
+			$this,
+			$options === NULL ? array('only_record_fields' => TRUE) : $options
+		);
+	}
+
+	/**
+	 * Ist der Datensatz als gelöscht markiert?
+	 * Wenn es keine Spalte oder TCA gibt, is es nie gelöscht!
+	 *
+	 * @return boolean
+	 */
+	public function isDeleted() {
+		$tableName = $this->getTableName();
+		$field = empty($GLOBALS['TCA'][$tableName]['ctrl']['delete'])
+			? 'deleted'
+			: $GLOBALS['TCA'][$tableName]['ctrl']['delete']
+		;
+		$value = $this->hasProperty($field) ? (int) $this->getProperty($field) : 0;
+		return $value > 0;
+	}
+
+	/**
+	 * Ist der Datensatz als gelöscht markiert?
+	 * Wenn es keine Spalte oder TCA gibt, is es nie gelöscht!
+	 *
+	 * @return boolean
+	 */
+	public function isHidden() {
+		$tableName = $this->getTableName();
+		$field = empty($GLOBALS['TCA'][$tableName]['ctrl']['enablecolumns']['disabled'])
+			? 'hidden'
+			: $GLOBALS['TCA'][$tableName]['ctrl']['enablecolumns']['disabled']
+		;
+		$value = $this->hasProperty($field) ? (int) $this->getProperty($field) : 0;
+		return $value > 0;
+	}
+
+	/**
+	 * Returns the record
+	 *
+	 * @return array
+	 */
+	function getRecord() {
+		return $this->getProperty();
+	}
+
+	/**
+	 * Liefert bei Tabellen, die im $TCA definiert sind,
+	 * die Namen der Tabellenspalten als Array.
+	 *
+	 * @return array mit Spaltennamen oder 0
+	 */
+	public function getColumnNames() {
+		$columns = $this->getTCAColumns();
+		return is_array($columns) ? array_keys($columns) : 0;
+	}
+
+	/**
+	 * Liefert die TCA-Definition der in der Tabelle definierten Spalten
+	 *
+	 * @return array
+	 */
+	public function getTCAColumns() {
+		$columns = tx_rnbase_util_TCA::getTcaColumns($this->getTableName());
+		return empty($columns) ? 0 : $columns;
+	}
 
 	/**
 	 * Liefert den Inhalt eine Spalte formatiert durch eine stdWrap. Per Konvention wird
