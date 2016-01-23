619b91198af76ed89e99cfcb1f530e6163682406
René Nitzsche <rene@system25.de>
2016-01-23 17:21:40 +0100

    Class definition for BE module base class within one single file

--- Classes/Backend/Module/Base.php
+++ Classes/Backend/Module/Base.php
@@ -3,7 +3,9 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 tx_rnbase::load('tx_rnbase_util_TYPO3');
 if (tx_rnbase_util_TYPO3::isTYPO60OrHigher()) {
-	require_once(tx_rnbase_util_Extensions::extPath('rn_base') . 'Classes/Backend/Module/BaseSince6.php');
+	class Tx_Rnbase_Backend_Module_Base extends \TYPO3\CMS\Backend\Module\BaseScriptClass {
+	}
 } else {
-	require_once(tx_rnbase_util_Extensions::extPath('rn_base') . 'Classes/Backend/Module/BaseUntil6.php');
+	class Tx_Rnbase_Backend_Module_Base extends t3lib_SCbase {
+	}
 }
