b568316aba8821633c7196981740d7583e454127
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2016-02-03 11:51:17 +0100

    #34 Abwärtskompatibilität wiederhergestellt

--- util/class.tx_rnbase_util_FormTool.php
+++ util/class.tx_rnbase_util_FormTool.php
@@ -29,7 +29,9 @@ tx_rnbase::load('Tx_Rnbase_Backend_Form_ToolBox');
  * im Backend zur Verfügung
  */
 class tx_rnbase_util_FormTool extends Tx_Rnbase_Backend_Form_ToolBox {
-
+	public function init($doc, $module=NULL) {
+		parent::init($doc, $module);
+	}
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rn_base/util/class.tx_rnbase_util_FormTool.php'])	{
