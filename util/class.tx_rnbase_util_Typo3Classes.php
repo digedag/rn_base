2c548fc9446034d1b7deed7804b827b427b3e1fc
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2016-03-22 11:52:05 +0100

    phpdoc added

--- util/class.tx_rnbase_util_Typo3Classes.php
+++ util/class.tx_rnbase_util_Typo3Classes.php
@@ -40,213 +40,214 @@ class tx_rnbase_util_Typo3Classes {
 	const HIGHER6 = 'higher6';
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\Messaging\FlashMessage
 	 */
 	public static function getFlashMessageClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_FlashMessage',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\Messaging\\FlashMessage'
+			self::LOWER6 => 't3lib_FlashMessage',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\Messaging\\FlashMessage'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\Messaging\FlashMessageQueue
 	 */
 	public static function getFlashMessageQueueClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_FlashMessageQueue',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\Messaging\\FlashMessageQueue'
+			self::LOWER6 => 't3lib_FlashMessageQueue',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\Messaging\\FlashMessageQueue'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Backend\Form\FormEngine
 	 */
 	public static function getBackendFormEngineClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_tceforms',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Backend\\Form\\FormEngine'
+			self::LOWER6 => 't3lib_tceforms',
+			self::HIGHER6 => 'TYPO3\\CMS\\Backend\\Form\\FormEngine'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\Utility\File\BasicFileUtility
 	 */
 	public static function getBasicFileUtilityClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_basicFileFunctions',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\Utility\\File\\BasicFileUtility'
+			self::LOWER6 => 't3lib_basicFileFunctions',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\Utility\\File\\BasicFileUtility'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\TypoScript\ExtendedTemplateService
 	 */
 	public static function getExtendedTypoScriptTemplateServiceClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_tsparser_ext',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\TypoScript\\ExtendedTemplateService'
+			self::LOWER6 => 't3lib_tsparser_ext',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\TypoScript\\ExtendedTemplateService'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer
 	 */
 	public static function getContentObjectRendererClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 'tslib_cObj',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer'
+			self::LOWER6 => 'tslib_cObj',
+			self::HIGHER6 => 'TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
 	 */
 	public static function getTypoScriptFrontendControllerClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 'tslib_fe',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Frontend\\Controller\\TypoScriptFrontendController'
+			self::LOWER6 => 'tslib_fe',
+			self::HIGHER6 => 'TYPO3\\CMS\\Frontend\\Controller\\TypoScriptFrontendController'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication
 	 */
 	public static function getFrontendUserAuthenticationClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 'tslib_feUserAuth',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Frontend\\Authentication\\FrontendUserAuthentication'
+			self::LOWER6 => 'tslib_feUserAuth',
+			self::HIGHER6 => 'TYPO3\\CMS\\Frontend\\Authentication\\FrontendUserAuthentication'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\Charset\CharsetConverter
 	 */
 	public static function getCharsetConverterClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_cs',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\Charset\\CharsetConverter'
+			self::LOWER6 => 't3lib_cs',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\Charset\\CharsetConverter'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\DataHandling\DataHandler
 	 */
 	public static function getDataHandlerClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_tcemain',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\DataHandling\\DataHandler'
+			self::LOWER6 => 't3lib_tcemain',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\DataHandling\\DataHandler'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMSBackend\Sprite\SpriteManager
 	 */
 	public static function getSpriteManagerClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_SpriteManager',
-			self::HIGHER6 	=> 'TYPO3\\CMS\Backend\\Sprite\\SpriteManager'
+			self::LOWER6 => 't3lib_SpriteManager',
+			self::HIGHER6 => 'TYPO3\\CMS\Backend\\Sprite\\SpriteManager'
 		));
 	}
 	/**
-	 * @return string
+	 * @TODO: why this returns the NullTimeTracker? Should return the TimeTracker!
+	 * @return string|TYPO3\CMS\Core\TimeTracker\NullTimeTracker
 	 */
 	public static function getTimeTrackClass() {
 		// TYPO3\\CMS\\Core\\TimeTracker\\TimeTracker
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_timeTrack',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\TimeTracker\\NullTimeTracker'
+			self::LOWER6 => 't3lib_timeTrack',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\TimeTracker\\NullTimeTracker'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\Utility\CommandUtility
 	 */
 	public static function getCommandUtilityClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_exec',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\Utility\\CommandUtility'
+			self::LOWER6 => 't3lib_exec',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\Utility\\CommandUtility'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\Mail\MailMessage
 	 */
 	public static function getMailMessageClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_mail_Message',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\Mail\\MailMessage'
+			self::LOWER6 => 't3lib_mail_Message',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\Mail\\MailMessage'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\Html\HtmlParser
 	 */
 	public static function getHtmlParserClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_parsehtml',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\Html\\HtmlParser'
+			self::LOWER6 => 't3lib_parsehtml',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\Html\\HtmlParser'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\Utility\GeneralUtility
 	 * @see Tx_Rnbase_Utility_T3General for better usage
 	 */
 	public static function getGeneralUtilityClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_div',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\Utility\\GeneralUtility'
+			self::LOWER6 => 't3lib_div',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\Utility\\GeneralUtility'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser
 	 */
 	public static function getTypoScriptParserClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_TSparser',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\TypoScript\\Parser\\TypoScriptParser'
+			self::LOWER6 => 't3lib_TSparser',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\TypoScript\\Parser\\TypoScriptParser'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Backend\Template\DocumentTemplate
 	 */
 	public static function getDocumentTemplateClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 'template',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Backend\\Template\\DocumentTemplate'
+			self::LOWER6 => 'template',
+			self::HIGHER6 => 'TYPO3\\CMS\\Backend\\Template\\DocumentTemplate'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\TypoScript\TemplateService
 	 */
 	public static function getTemplateServiceClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_TStemplate',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\TypoScript\\TemplateService'
+			self::LOWER6 => 't3lib_TStemplate',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\TypoScript\\TemplateService'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Core\Utility\HttpUtility
 	 */
 	public static function getHttpUtilityClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 't3lib_utility_Http',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Core\\Utility\\HttpUtility'
+			self::LOWER6 => 't3lib_utility_Http',
+			self::HIGHER6 => 'TYPO3\\CMS\\Core\\Utility\\HttpUtility'
 		));
 	}
 
 	/**
-	 * @return string
+	 * @return string|TYPO3\CMS\Backend\Template\DocumentTemplate
 	 */
 	public static function getMediumDocumentTemplateClass() {
 		return self::getClassByCurrentTypo3Version(array(
-			self::LOWER6	=> 'mediumDoc',
-			self::HIGHER6 	=> 'TYPO3\\CMS\\Backend\\Template\\DocumentTemplate'
+			self::LOWER6 => 'mediumDoc',
+			self::HIGHER6 => 'TYPO3\\CMS\\Backend\\Template\\DocumentTemplate'
 		));
 	}
 
