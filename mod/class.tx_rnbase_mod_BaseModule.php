a7a8bd5adfe75a12a81862d4a0faf1959eb0b17b
Mario Seidel <mario.seidel@dmk-ebusiness.de>
2016-05-25 20:43:24 +0200

    fix variable name

--- mod/class.tx_rnbase_mod_BaseModule.php
+++ mod/class.tx_rnbase_mod_BaseModule.php
@@ -482,11 +482,11 @@ abstract class tx_rnbase_mod_BaseModule extends Tx_Rnbase_Backend_Module_Base im
 			);
 			$flashMessageService->getMessageQueueByIdentifier()->addMessage($message);
 		} else {
-			$flashMessageClass = tx_rnbase::makeInstance(
+			$flashMessageQueue = tx_rnbase::makeInstance(
 				tx_rnbase_util_Typo3Classes::getFlashMessageQueueClass(),
 				md5($message . $title . $severity)
 			);
-			$flashMessageClass->addMessage($message);
+			$flashMessageQueue->addMessage($message);
 		}
 	}
 
