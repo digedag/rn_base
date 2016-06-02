5b4d2f81c2edef38e877afb98e273d87975bb0fc
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2016-06-02 12:57:11 +0200

    added default array_object option to tests

--- tests/Classes/Domain/Repository/AbstractRepositoryTest.php
+++ tests/Classes/Domain/Repository/AbstractRepositoryTest.php
@@ -1,34 +1,37 @@
 <?php
 /***************************************************************
- *  Copyright notice
+ * Copyright notice
  *
- * (c) 2015 René Nitzsche <rene@system25.de>
+ * (c) 2015-2016 René Nitzsche <rene@system25.de>
  * All rights reserved
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
  *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This copyright notice MUST APPEAR in all copies of the script!
+ * This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
 tx_rnbase::load('Tx_Rnbase_Domain_Repository_AbstractRepository');
 
 /**
+ * Test for abstract repository
+ *
  * @package TYPO3
  * @subpackage Tx_Rnbase
+ * @author Michael Wagner
  */
-class Tx_Rnbase_Repository_AbstractRepository_testcase
+class Tx_Rnbase_Domain_Model_AbstractRepositoryTest
 	extends tx_rnbase_tests_BaseTestCase {
 
 	protected function setUp() {
@@ -124,7 +127,7 @@ class Tx_Rnbase_Repository_AbstractRepository_testcase
 		$repository = $this->getRepositoryMock(array('getSearchClass', 'getWrapperClass', 'getSearcher'));
 
 		$fields = array('someField' => 1);
-		$options = array('enablefieldsbe' => 1);
+		$options = array('array_object' => true, 'enablefieldsbe' => 1);
 
 		$searcher = $this->getMock(
 			'tx_rnbase_util_SearchGeneric',
--- tests/Classes/Domain/Repository/PersistenceRepositoryTest.php
+++ tests/Classes/Domain/Repository/PersistenceRepositoryTest.php
@@ -1,33 +1,34 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
- *  (c) 2016 Rene Nitzsche (rene@system25.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ * Copyright notice
+ *
+ * (c) 2016 René Nitzsche <rene@system25.de>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 tx_rnbase::load('tx_rnbase_tests_BaseTestCase');
 
 /**
  * Test for persistence repository
- * @package tx_rnbase
- * @subpackage tx_rnbase_tests
+ *
+ * @package TYPO3
+ * @subpackage Tx_Rnbase
  * @author Michael Wagner
  */
 class Tx_Rnbase_Domain_Model_PersistenceRepositoryTest
