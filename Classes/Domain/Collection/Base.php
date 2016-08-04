d9b9d2db42a8f907588e2566bfbeb7bca1c80369
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2016-08-04 09:48:21 +0200

    #52 Collections added

---
+++ Classes/Domain/Collection/Base.php
@@ -0,0 +1,49 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2016 Rene Nitzsche <rene@system25.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+tx_rnbase::load('Tx_Rnbase_Utility_Composer');
+Tx_Rnbase_Utility_Composer::autoload();
+
+/**
+ * Only a wrapper for doctrine array collection
+ *
+ * @package TYPO3
+ * @subpackage rn_base
+ * @author Michael Wagner
+ */
+class Tx_Rnbase_Domain_Collection_Base
+	extends \Doctrine\Common\Collections\ArrayCollection
+{
+	/**
+	 * Only a wrapper for add
+	 *
+	 * @param mixed $value The element to add
+	 *
+	 * @return bool Always true
+	 */
+	public function append($value)
+	{
+		return $this->add($value);
+	}
+}
---
+++ Classes/Utility/Composer.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright notice
+ *
+ * (c) 2015 DMK E-Business GmbH <dev@dmk-ebusiness.de>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+
+/**
+ * extension configs
+ *
+ * @package TYPO3
+ * @subpackage Tx_T3rest
+ * @author Michael Wagner
+ */
+final class Tx_Rnbase_Utility_Composer
+{
+	static $loaded = FALSE;
+
+	/**
+	 * preloads the
+	 *
+	 * @return void
+	 */
+	public static function autoload()
+	{
+		if (self::$loaded === TRUE) {
+			return;
+		}
+		require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath(
+			'rn_base',
+			'Resources/Private/PHP/Composer/autoload.php'
+		);
+		self::$loaded = TRUE;
+	}
+
+}
