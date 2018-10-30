be53b880c7441813cc6c63c9085596c6784829f8
digedag <rene@system25.de>
2018-10-30 11:31:09 +0000

    Apply fixes from StyleCI

--- tests/Classes/Controller/ExtbaseControllerWithCacheTagsTraitTest.php
+++ tests/Classes/Controller/ExtbaseControllerWithCacheTagsTraitTest.php
@@ -35,7 +35,7 @@ use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
  * @license         http://www.gnu.org/licenses/lgpl.html
  *                  GNU Lesser General Public License, version 3 or later
  */
-class CacheTagsTraitTest extends \tx_rnbase_tests_BaseTestCase
+class ExtbaseControllerWithCacheTagsTraitTest extends \tx_rnbase_tests_BaseTestCase
 {
 
     /**
