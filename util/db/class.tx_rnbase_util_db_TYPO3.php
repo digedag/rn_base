883670a286b2a6fcc1103390f0e548b969326954
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2018-12-13 22:43:14 +0100

    add lagacy db conection driver for new connection pool #121

--- util/db/class.tx_rnbase_util_db_TYPO3.php
+++ util/db/class.tx_rnbase_util_db_TYPO3.php
@@ -25,7 +25,7 @@
 /**
  * DB wrapper for TYPO3 database
  */
-class tx_rnbase_util_db_TYPO3
+class tx_rnbase_util_db_TYPO3 implements tx_rnbase_util_db_IDatabase, tx_rnbase_util_db_IDatabaseT3
 {
     /**
      * Creates a SELECT SQL-statement
@@ -40,7 +40,7 @@ class tx_rnbase_util_db_TYPO3
      */
     public function SELECTquery($select_fields, $from_table, $where_clause, $groupBy = '', $orderBy = '', $limit = '')
     {
-        return $GLOBALS['TYPO3_DB']->SELECTquery($what, $fromClause, $where, $groupBy, $orderBy, $limit);
+        return $GLOBALS['TYPO3_DB']->SELECTquery($select_fields, $from_table, $where_clause, $groupBy, $orderBy, $limit);
     }
 
     /**
@@ -56,7 +56,7 @@ class tx_rnbase_util_db_TYPO3
      */
     public function exec_SELECTquery($select_fields, $from_table, $where_clause, $groupBy = '', $orderBy = '', $limit = '')
     {
-        return $GLOBALS['TYPO3_DB']->exec_SELECTquery($what, $fromClause, $where, $groupBy, $orderBy, $limit);
+        return $GLOBALS['TYPO3_DB']->exec_SELECTquery($select_fields, $from_table, $where_clause, $groupBy, $orderBy, $limit);
     }
     /**
      * Creates and executes an INSERT SQL-statement for $table from the array with field/value pairs $fields_values.
@@ -155,5 +155,97 @@ class tx_rnbase_util_db_TYPO3
     public function sql_free_result($res)
     {
         return $GLOBALS['TYPO3_DB']->sql_free_result($res);
+    }
+
+    /**
+     * Returns the number of rows affected by the last INSERT, UPDATE or DELETE query
+     * mysql_affected_rows() wrapper function
+     *
+     * @return  int     Number of rows affected by last query
+     */
+    public function sql_affected_rows()
+    {
+        return $GLOBALS['TYPO3_DB']->sql_affected_rows();
+    }
+
+    /**
+     * Get the ID generated from the previous INSERT operation
+     * mysql_insert_id() wrapper function
+     *
+     * @return  int     The uid of the last inserted record.
+     */
+    public function sql_insert_id()
+    {
+        return $GLOBALS['TYPO3_DB']->sql_insert_id();
+    }
+
+    /**
+     * Returns the error status on the last sql() execution
+     * mysql_error() wrapper function
+     *
+     * @return  string      MySQL error string.
+     */
+    public function sql_error()
+    {
+        return $GLOBALS['TYPO3_DB']->sql_error();
+    }
+
+    /**
+     * Substitution for PHP function "addslashes()"
+     *
+     * Use this function instead of the PHP addslashes() function when you build queries
+     * this will prepare your code for DBAL.
+     * NOTICE: You must wrap the output of this function in SINGLE QUOTES to be DBAL compatible.
+     * Unless you have to apply the single quotes yourself you should rather use ->fullQuoteStr()!
+     *
+     * @param string $str Input string
+     * @param string $table Table name for which to quote string.
+     * @return string Output string; Quotes (" / ') and \ will be backslashed (or otherwise based on DBAL handler)
+     */
+    public function quoteStr($str, $table)
+    {
+        return $GLOBALS['TYPO3_DB']->quoteStr($str, $table);
+    }
+
+    /**
+     * Escaping values for SQL LIKE statements.
+     *
+     * @param string $str Input string
+     * @param string $table Table name for which to escape string.
+     *
+     * @return string Output string; % and _ will be escaped with \ (or otherwise based on DBAL handler)
+     */
+    public function escapeStrForLike($str, $table)
+    {
+        return $GLOBALS['TYPO3_DB']->escapeStrForLike($str, $table);
+    }
+
+    /**
+     * Will fullquote all values in the one-dimensional array so they are ready to "implode" for an sql query.
+     *
+     * @param array $arr Array with values (either associative or non-associative array)
+     * @param string $table Table name for which to quote
+     * @param bool|array|string $noQuote List/array of keys NOT to quote (eg. SQL functions) - ONLY for associative arrays
+     * @param bool $allowNull Whether to allow NULL values
+     *
+     * @return array The input array with the values quoted
+     */
+    public function fullQuoteArray($arr, $table, $noQuote = false, $allowNull = false)
+    {
+        return $GLOBALS['TYPO3_DB']->fullQuoteArray($arr, $table, $noQuote, $allowNull);
+    }
+
+    /**
+     * Escaping and quoting values for SQL statements.
+     *
+     * @param string $str Input string
+     * @param string $table Table name for which to quote string.
+     * @param bool $allowNull Whether to allow NULL values
+     *
+     * @return string Output string
+     */
+    public function fullQuoteStr($str, $table, $allowNull = false)
+    {
+        return $GLOBALS['TYPO3_DB']->fullQuoteStr($str, $table, $allowNull);
     }
 }
