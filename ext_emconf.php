8f6f1bb782f5f2ace2f93ab5bbfb6118c1f56339
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2019-03-15 15:18:48 +0100

    set requirement for TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-8.7.99',
+            'typo3' => '6.2.0-9.5.99',
             'php' => '5.6.0-8.9.99'
         ),
         'conflicts' => array(
