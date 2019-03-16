b652b0843c3adb136cb75d3f7052f62c64169e50
René Nitzsche <rene@system25.de>
2019-03-16 21:50:06 +0100

    autoload entry for config processor

--- ext_autoload.php
+++ ext_autoload.php
@@ -40,6 +40,7 @@ return array(
     'tx_rnbase_cache_typo3cache62' => $extensionPath . 'cache/class.tx_rnbase_cache_TYPO3Cache62.php',
     'tx_rnbase_commandline_controller' => $extensionClassesPath . 'CommandLine/Controller.php',
     'tx_rnbase_configurations' => $extensionPath . 'class.tx_rnbase_configurations.php',
+    'tx_rnbase_configuration_processor' => $extensionPath . 'Classes/Configuration/Processor.php',
     'tx_rnbase_controller' => $extensionPath . 'class.tx_rnbase_controller.php',
     'tx_rnbase_database_connection' => $extensionClassesPath . 'Database/Connection.php',
     'tx_rnbase_database_connectionmock' => $extensionPath . 'tests/util/class.tx_rnbase_tests_util_DB_testcase.php',
