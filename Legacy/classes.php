0b89b9a5409b8f8e0540ef27e6b63f2e3a87d54c
digedag <rene@system25.de>
2019-11-24 17:30:10 +0000

    Apply fixes from StyleCI

--- Legacy/classes.php
+++ Legacy/classes.php
@@ -1,9 +1,14 @@
 <?php
 
 /** @deprecated */
-interface Tx_Rnbase_Configuration_ProcessorInterface extends \Sys25\RnBase\Configuration\ConfigurationInterface {};
+interface Tx_Rnbase_Configuration_ProcessorInterface extends \Sys25\RnBase\Configuration\ConfigurationInterface
+{
+};
 /** @deprecated */
-class tx_rnbase_configurations extends Sys25\RnBase\Configuration\Processor {};
+class tx_rnbase_configurations extends Sys25\RnBase\Configuration\Processor
+{
+};
 /** @deprecated */
-class Tx_Rnbase_Configuration_Processor extends tx_rnbase_configurations {};
-
+class Tx_Rnbase_Configuration_Processor extends tx_rnbase_configurations
+{
+};
