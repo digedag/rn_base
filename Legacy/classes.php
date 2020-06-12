3a7379996b79de8b4e1bfa69da15382d9b4d9551
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2020-06-12 10:17:17 +0200

    bugfix for legacy filter item
    restored the old filter item class which is using the
    old filter item interface. If the old filter item
    class would only inherit from the new one we wouldn't
    have the old filter item interface but the new one.
    That would lead to problems when using
    BaseFilter:addFilterItem as this methods expects an object
    with the old filter item interface class.

--- Legacy/classes.php
+++ Legacy/classes.php
@@ -91,7 +91,3 @@ trait Tx_Rnbase_Configuration_ConfigurableTrait
 interface tx_rnbase_IFilterItem extends Sys25\RnBase\Frontend\Filter\IFilterItem
 {
 }
-/** @deprecated */
-class tx_rnbase_FilterItem extends Sys25\RnBase\Frontend\Filter\FilterItem
-{
-}
