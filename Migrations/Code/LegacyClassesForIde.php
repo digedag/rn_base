c3e4f4bf34d193f233a91e97b89013f80b646831
Oliver Klee <typo3-coding@oliverklee.de>
2021-10-16 11:51:31 +0200

    [BUGFIX] Move `AbstractController` into its own file (#257)
    This class has no alias and has some code of its own. So it
    needs to reside in its own autoloaded file instead of being only
    stubs for the IDE.

--- Migrations/Code/LegacyClassesForIde.php
+++ Migrations/Code/LegacyClassesForIde.php
@@ -434,35 +434,3 @@ namespace Sys25\RnBase\Search {
     {
     }
 }
-
-namespace Sys25\RnBase\Controller {
-    /**
-     * @deprecated
-     */
-    abstract class AbstractController extends \tx_rnbase_action_BaseIOC
-    {
-        protected function handleRequest(&$parameters, &$configurations, &$viewdata)
-        {
-            return $this->doRequest();
-        }
-
-        abstract protected function doRequest();
-
-        protected function getViewClassName()
-        {
-            return \Sys25\RnBase\Fluid\View\Action::class;
-        }
-
-        protected function assignToView($name, $data)
-        {
-            $this->getViewData()->offsetSet($name, $data);
-
-            return $this;
-        }
-
-        protected function getConfigurationValue($confId)
-        {
-            return $this->getConfigurations()->get($this->getConfId().$confId);
-        }
-    }
-}
