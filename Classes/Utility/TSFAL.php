ce735128d8292c15a285ec585263004e7caa22ac
Oliver Klee <typo3-coding@oliverklee.de>
2021-12-16 20:08:08 +0100

    [TASK] Drop pre-8.7 code from `TSFAL`

--- Classes/Utility/TSFAL.php
+++ Classes/Utility/TSFAL.php
@@ -452,8 +452,7 @@ class TSFAL
         $allowedFileExtensions = (string) $options['config']['allowedFileExtensions'];
         $disallowedFileExtensions = (string) $options['config']['disallowedFileExtensions'];
         if ('image' == $type) {
-            $ttContentLocallang = TYPO3::isTYPO87OrHigher() ? 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf' :
-                'LLL:EXT:cms/locallang_ttc.xlf';
+            $ttContentLocallang = 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf';
             $types = self::buildMediaPalette();
             $customSettingOverride = array_merge(
                 [
@@ -509,8 +508,7 @@ class TSFAL
 
     private static function buildMediaPalette()
     {
-        $commonTcaLocallang = TYPO3::isTYPO87OrHigher() ? 'LLL:EXT:lang/Resources/Private/Language/locallang_tca.xlf' :
-            'LLL:EXT:lang/locallang_tca.xlf';
+        $commonTcaLocallang = 'LLL:EXT:lang/Resources/Private/Language/locallang_tca.xlf';
         $paletteLabel = TYPO3::isTYPO95OrHigher() ? '' : $commonTcaLocallang.':sys_file_reference.imageoverlayPalette';
         $typeDefs = [
             '0',
