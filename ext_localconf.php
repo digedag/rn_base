0e5a6756aaa0d379b4a3e2759fe1245216aaab67
René Nitzsche <rene@system25.de>
2023-06-18 12:22:09 +0200

    Bugfix/missings constants non composer mode (#316)
    * Fix missing constants in non-composer-mode

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,9 @@ if (!(defined('TYPO3') || defined('TYPO3_MODE'))) {
     exit('Access denied.');
 }
 
+// Necessary for non composer installs
+require Sys25\RnBase\Utility\Extensions::extPath('rn_base').'Classes/Constants.php';
+
 if (empty($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['rnbase']) &&
     Sys25\RnBase\Configuration\Processor::getExtensionCfgValue('rn_base', 'activateCache')) {
     Sys25\RnBase\Cache\CacheManager::registerCache(
