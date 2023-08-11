4f906ceca69f6a71c8644213dda1d8823245a06d
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2023-08-11 14:33:10 +0200

    Bugfix fix undefined array keys

--- Classes/Search/SearchBase.php
+++ Classes/Search/SearchBase.php
@@ -493,7 +493,7 @@ abstract class SearchBase
     {
         $genericJoins = null;
         if ($this->isGeneric()) {
-            $aliasArr = $this->genericData['alias'];
+            $aliasArr = $this->genericData['alias'] ?? null;
             if (is_array($aliasArr)) {
                 foreach ($aliasArr as $alias => $data) {
                     $makeJoin = isset($tableAliases[$alias]);
